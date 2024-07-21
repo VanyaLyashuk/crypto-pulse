046287e2cf5f3f23de044304faadb7df48caa3de
Ivan Lyashuk <ivn@Ivans-MacBook-Pro.local>
2024-07-21 08:34:43 +0300

    feat: add methods for fetching historical chart data and combining with market data

--- src/services/CoinGeckoService.ts
+++ src/services/CoinGeckoService.ts
@@ -1,11 +1,22 @@
-import { ICoinsMarketData, ITransformedCoinsMarketData } from "../models";
-import { transformCoinsListWithMarketData } from "./CoinGeckoServiceUtils";
+import {
+  ICoinHistoricalChartData,
+  ICoinsMarketData,
+  ICoinWithHistoricalData,
+  ITransformedCoinHistoricalChartData,
+  ITransformedCoinsMarketData
+} from "../models";
+import {
+  transformCoinHistoricalChartData,
+  transformCoinsListWithMarketData,
+} from "./CoinGeckoServiceUtils";
 
 class CoinGeckoService {
-  private readonly _apiBase: string = "https://api.coingecko.com/api/v3/";
   private readonly _apiKey: string = import.meta.env.VITE_COINGECKO_API_KEY;
+  private readonly _apiBase: string = "https://api.coingecko.com/api/v3/";
   private readonly _vsCurrency: string = "usd";
-  private readonly _perPage: number = 50;
+  private readonly _perPage: number = 30;
+  private readonly _historicalChartDays: number = 7;
+  private readonly _historicalChartInterval: string = "daily";
 
   private async getResource<T>(url: string): Promise<T> {
     const options = {
@@ -36,6 +47,41 @@ class CoinGeckoService {
     const transformedData = transformCoinsListWithMarketData(data);
 
     return transformedData;
+  }
+
+  public async _getCoinHistoricalChartDataById(
+    id: string,
+    vsCurrency: string = this._vsCurrency,
+    days: number = this._historicalChartDays,
+    interval: string = this._historicalChartInterval,
+  ): Promise<ITransformedCoinHistoricalChartData> {
+    const url = `${this._apiBase}coins/${id}/market_chart?vs_currency=${vsCurrency}&days=${days}&interval=${interval}`;
+    const data = await this.getResource<ICoinHistoricalChartData>(url);
+    const transformedData = transformCoinHistoricalChartData(data);
+
+    return transformedData;
+  }
+
+  public async _getCoinsListWithHistoricalData(
+    vsCurrency: string = this._vsCurrency,
+    perPage: number = this._perPage,
+    days: number = this._historicalChartDays,
+    interval: string = this._historicalChartInterval
+  ): Promise<ICoinWithHistoricalData[]> {
+    const coinsMarketData = await this._getCoinsListWithMarketData(vsCurrency, perPage);
+
+    const coinsWithHistoricalData = await Promise.all(
+      coinsMarketData.map(async (coin) => {
+        const historicalData = await this._getCoinHistoricalChartDataById(coin.id, vsCurrency, days, interval);
+
+        return {
+          ...coin,
+          historicalPriceData: historicalData.prices
+        } as ICoinWithHistoricalData;
+      })
+    );
+
+    return coinsWithHistoricalData;
   }
 }
 
