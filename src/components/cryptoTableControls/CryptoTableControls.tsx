5847e6bd844884a329f0e981d83e0e20ebe9ac15
Ivan Lyashuk <ivn@Ivans-MacBook-Pro.local>
2024-08-05 15:33:29 +0300

    feat(CryptoTableControls): add support for rendering children components

--- src/components/cryptoTableControls/CryptoTableControls.tsx
+++ src/components/cryptoTableControls/CryptoTableControls.tsx
@@ -1,17 +1,11 @@
 import React from "react";
-import { ICryptoTableControlsProps } from "../../models";
-import CryptoTableRowsPerPage from "../cryptoTableRowsPerPage/CryptoTableRowsPerPage";
 
-const CryptoTableControls: React.FC<ICryptoTableControlsProps> = ({
-  rowsPerPage,
-  onRowsChange,
+const CryptoTableControls: React.FC<React.PropsWithChildren<{}>> = ({
+  children,
 }) => {
   return (
-    <div className="flex justify-end w-full pt-4 px-2 max-w-[1300px] m-auto">
-      <CryptoTableRowsPerPage
-        rowsPerPage={rowsPerPage}
-        onRowsChange={onRowsChange}
-      />
+    <div className="flex justify-center w-full px-2 max-w-[1300px] m-auto">
+      {children}
     </div>
   );
 };
