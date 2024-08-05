3a63129bbe4411ee4d76de29f0bc2108129d7381
Ivan Lyashuk <ivn@Ivans-MacBook-Pro.local>
2024-08-05 15:20:58 +0300

    refactor(PriceChangeCell): add dash character for missing data

--- src/components/cryptoTable/PriceChangeCell.tsx
+++ src/components/cryptoTable/PriceChangeCell.tsx
@@ -9,14 +9,19 @@ const PriceChangeCell: React.FC<ITransformedPriceChange> = ({
   isNegative,
 }) => {
   const cellClasses = clsx("flex items-center justify-end gap-0.5", {
-    "text-red-600": isNegative,
-    "text-green-500": !isNegative,
+    "text-red-600": isNegative && value !== '-',
+    "text-green-500": !isNegative && value !== '-',
+    "text-gray-700": value === '-' && isNegative,
   });
 
   return (
-    <div className={cellClasses}>
-      <ArrowIcon isNegative={isNegative} />
-      <span>{value}</span>
+    <div className={cellClasses}>{
+      value === '-' ? '-' : 
+      (<>
+        <ArrowIcon isNegative={isNegative} />
+        <span>{value}</span>
+      </>)
+    }
     </div>
   );
 };
