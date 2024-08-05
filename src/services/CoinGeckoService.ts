b2c9fc04c0cb5ee948821fa9deddcd1d12f5fe40
Ivan Lyashuk <ivn@Ivans-MacBook-Pro.local>
2024-08-05 15:37:22 +0300

    feat(CoinGeckoService): add function to fetch coins list length

--- src/services/CoinGeckoService.ts
+++ src/services/CoinGeckoService.ts
@@ -1,11 +1,12 @@
-import { ICoinsMarketData, ITransformedCoinsMarketData } from "../models";
+import { ICoinsListData, ICoinsMarketData, ITransformedCoinsMarketData } from "../models";
 import { transformCoinsListWithMarketData } from "./CoinGeckoServiceUtils";
 
 class CoinGeckoService {
   private readonly _apiKey: string = import.meta.env.VITE_COINGECKO_API_KEY;
   private readonly _apiBase: string = "https://api.coingecko.com/api/v3/";
   private readonly _vsCurrency: string = "usd";
-  private readonly _perPage: number = 30;
+  private readonly _perPage: number = 10;
+  private readonly _page: number = 1;
 
   private async getResource<T>(url: string): Promise<T> {
     const options = {
@@ -28,15 +29,22 @@ class CoinGeckoService {
 
   public async _getCoinsListWithMarketData(
     vsCurrency: string = this._vsCurrency,
-    perPage: number = this._perPage
+    perPage: number = this._perPage,
+    page: number = this._page
   ): Promise<ITransformedCoinsMarketData[]> {
-    const url = `${this._apiBase}coins/markets?vs_currency=${vsCurrency}&per_page=${perPage}&sparkline=true&price_change_percentage=1h%2C24h%2C7d&precision=2`;
-
-
+    const url = `${this._apiBase}coins/markets?vs_currency=${vsCurrency}&per_page=${perPage}&page=${page}&sparkline=true&price_change_percentage=1h%2C24h%2C7d&precision=2`;
     const data = await this.getResource<ICoinsMarketData[]>(url);
     const transformedData = transformCoinsListWithMarketData(data);
 
     return transformedData;
+  }
+
+  public async _getCoinsListLength(
+  ): Promise<number> {
+    const url = `${this._apiBase}coins/list`;
+    const data = await this.getResource<ICoinsListData[]>(url);
+
+    return data.length;
   }
 }
 
