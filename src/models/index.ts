0148069be7cf28d36535348dcccaa8ceca7d31c5
Ivan Lyashuk <ivn@Ivans-MacBook-Pro.local>
2024-08-05 15:11:30 +0300

    refactor: update models index file with latest changes

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,16 +1,23 @@
+import { ICoinsListData } from "./CoinsListData.interface";
 import { ICoinsMarketData } from "./CoinsMarketData.interface";
-import { ICryptoTableControlsProps } from "./CryptoTableControlsProps.interface";
 import { ICryptoTableRowsPerPageProps } from "./CryptoTableRowsPerPageProps.interface";
 import { ICryptoTableSkeletonProps } from "./CryptoTableSkeletonProps.interface";
+import { IPaginationProps } from "./Pagination.interface";
+import { TPaginationButton } from "./PaginationButton.type";
+import { IPaginationComponentProps } from "./PaginationComponentProps.interface";
 import { ISparklineChartProps } from "./SparklineChartProps.interface";
 import { ITransformedCoinsMarketData } from "./TransformedCoinsMarketData.interface";
 import { ITransformedPriceChange } from "./TransformedPriceChange.interface";
 
 export type {
-    ICoinsMarketData,
-    ICryptoTableControlsProps, ICryptoTableRowsPerPageProps, ICryptoTableSkeletonProps,
+    ICoinsListData, ICoinsMarketData,
+    ICryptoTableRowsPerPageProps,
+    ICryptoTableSkeletonProps,
+    IPaginationComponentProps,
+    IPaginationProps,
     ISparklineChartProps,
     ITransformedCoinsMarketData,
-    ITransformedPriceChange
+    ITransformedPriceChange,
+    TPaginationButton
 };
 
