b4237cf106656e21b1c2c9a5e00a07d7052b8d3d
Ivan Lyashuk <ivn@Ivans-MacBook-Pro.local>
2024-08-11 23:22:19 +0300

    feat(CryptoTable): add error state and error component, extract markup in view component

--- src/components/cryptoTable/CryptoTable.tsx
+++ src/components/cryptoTable/CryptoTable.tsx
@@ -1,26 +1,17 @@
-import {
-  Cell,
-  ColumnDef,
-  flexRender,
-  getCoreRowModel,
-  getSortedRowModel,
-  useReactTable,
-} from "@tanstack/react-table";
 import clsx from "clsx";
-import React, { useCallback, useEffect, useMemo, useState } from "react";
-import { BiSolidDownArrow, BiSolidUpArrow } from "react-icons/bi";
-import { FaRegStar } from "react-icons/fa";
+import React, { useCallback, useEffect, useState } from "react";
 import {
   ICryptoTableRowsPerPageProps,
+  ICryptoTableViewProps,
   ITransformedCoinsMarketData,
 } from "../../models";
 import CoinGeckoService from "../../services/CoinGeckoService";
 import CryptoTableControls from "../cryptoTableControls/CryptoTableControls";
 import CryptoTableRowsPerPage from "../cryptoTableRowsPerPage/CryptoTableRowsPerPage";
 import CryptoTableSkeleton from "../cryptoTableSkeleton/CryptoTableSkeleton";
+import CryptoTableView from "../cryptoTableView/CryptoTableView";
+import ErrorMessage from "../errorMessage/ErrorMessage";
 import Pagination from "../pagination/Pagination";
-import PriceChangeCell from "./PriceChangeCell";
-import SparklineChart from "./SparklineChart";
 
 const CryptoTable: React.FC = () => {
   const [coins, setCoins] = useState<ITransformedCoinsMarketData[]>([]);
@@ -31,7 +22,8 @@ const CryptoTable: React.FC = () => {
   const [lastPage, setLastPage] = useState<number>(0);
   const [loading, setLoading] = useState<boolean>(true);
   const [loadingDelay, setLoadingDelay] = useState<boolean>(true);
-  
+  const [error, setError] = useState(false);
+
   const rowsSelectOptions: number[] = [10, 30, 50, 100];
 
   const coinGeckoService = new CoinGeckoService();
@@ -68,9 +60,19 @@ const CryptoTable: React.FC = () => {
     }
   }, [loading]);
 
-  const onCoinsDataRequest = () => {
+  const onLoading = () => {
     setLoading(true);
     setLoadingDelay(true);
+    setError(false);
+  };
+
+  const onError = () => {
+    setLoading(false);
+    setError(true);
+  };
+
+  const onCoinsDataRequest = () => {
+    onLoading();
 
     coinGeckoService
       ._getCoinsListWithMarketData("usd", rowsPerPage, currentPage)
@@ -80,13 +82,12 @@ const CryptoTable: React.FC = () => {
       })
       .catch((error) => {
         console.error("Error fetching coins data:", error);
-        setLoading(false);
+        onError();
       });
   };
 
   const onTotalCoinsRequest = () => {
-    setLoading(true);
-    setLoadingDelay(true);
+    onLoading();
 
     coinGeckoService
       ._getCoinsListLength()
@@ -96,11 +97,11 @@ const CryptoTable: React.FC = () => {
       })
       .catch((error) => {
         console.error("Error fetching total coins data:", error);
-        setLoading(false);
+        onError();
       });
   };
 
-  const toggleRowsSelect = useCallback(
+  const onToggleRowsSelect = useCallback(
     () => setIsRowsSelectOpen(!isRowsSelectOpen),
     [isRowsSelectOpen, setIsRowsSelectOpen]
   );
@@ -113,127 +114,9 @@ const CryptoTable: React.FC = () => {
       [setRowsPerPage]
     );
 
-  const columns: ColumnDef<ITransformedCoinsMarketData>[] = useMemo(
-    () => [
-      {
-        header: () => "",
-        id: "favorite",
-        cell: () => (
-          <button>
-            <FaRegStar />
-          </button>
-        ),
-        size: 32,
-        enableSorting: false,
-      },
-      {
-        header: "#",
-        accessorKey: "market_cap_rank",
-        size: 32,
-        minSize: 32,
-        maxSize: 125,
-        enableSorting: true,
-      },
-      {
-        header: "Coin",
-        accessorKey: "name",
-        cell: ({ row }) => (
-          <div className="flex items-center">
-            <img
-              src={row.original.image}
-              alt={row.original.name}
-              className="w-6 h-6 mr-2"
-              style={{ flexShrink: 0 }}
-            />
-            <div className="overflow-hidden break-words">
-              <span className="font-medium">{row.original.name}</span>{" "}
-              <span className="text-gray-500 uppercase">
-                {row.original.symbol}
-              </span>
-            </div>
-          </div>
-        ),
-        size: 500,
-        minSize: 200,
-        maxSize: 500,
-        enableSorting: true,
-      },
-      { header: "Price", accessorKey: "current_price", enableSorting: true },
-      {
-        header: "1h",
-        accessorKey: "price_change_percentage_1h_in_currency",
-        cell: ({ row }) => {
-          const { value, isNegative } =
-            row.original.price_change_percentage_1h_in_currency;
-          return <PriceChangeCell value={value} isNegative={isNegative} />;
-        },
-        size: 70,
-        minSize: 70,
-        maxSize: 125,
-        enableSorting: true,
-      },
-      {
-        header: "24h",
-        accessorKey: "price_change_percentage_24h_in_currency",
-        cell: ({ row }) => {
-          const { value, isNegative } =
-            row.original.price_change_percentage_24h_in_currency;
-          return <PriceChangeCell value={value} isNegative={isNegative} />;
-        },
-        size: 70,
-        minSize: 70,
-        maxSize: 125,
-        enableSorting: true,
-      },
-      {
-        header: "7d",
-        accessorKey: "price_change_percentage_7d_in_currency",
-        cell: ({ row }) => {
-          const { value, isNegative } =
-            row.original.price_change_percentage_7d_in_currency;
-          return <PriceChangeCell value={value} isNegative={isNegative} />;
-        },
-        size: 70,
-        minSize: 70,
-        maxSize: 125,
-        enableSorting: true,
-      },
-      {
-        header: "24h Volume",
-        accessorKey: "total_volume",
-        size: 150,
-        minSize: 150,
-        maxSize: 400,
-        enableSorting: true,
-      },
-      {
-        header: "Market Cap",
-        accessorKey: "market_cap",
-        size: 110,
-        minSize: 110,
-        maxSize: 400,
-        enableSorting: true,
-      },
-      {
-        header: "Last 7 Days",
-        accessorKey: "sparkline_in_7d",
-        cell: ({ row }) => {
-          const { price } = row.original.sparkline_in_7d;
-
-          return <SparklineChart price={price} />;
-        },
-        size: 151,
-        minSize: 151,
-        maxSize: 151,
-        enableSorting: false,
-      },
-    ],
-    []
-  );
-
-  const getCellClasses = (
-    cell: Cell<ITransformedCoinsMarketData, unknown>,
-    index: number
+  const getCellClasses: ICryptoTableViewProps["getCellClasses"] = (
+    cell,
+    index
   ) => {
     return clsx("p-2 text-sm text-gray-700 bg-white", {
       "w-8": cell.column.id === "favorite",
@@ -242,105 +125,45 @@ const CryptoTable: React.FC = () => {
     });
   };
 
-  const table = useReactTable({
-    data: coins,
-    columns,
-    getCoreRowModel: getCoreRowModel(),
-    getSortedRowModel: getSortedRowModel(),
-    enableSortingRemoval: true,
-  });
+  const errMsg = error ? <ErrorMessage message="Ooops! Something went wrong!" /> : null;
+  const skeleton =
+    loading || (loadingDelay && !error) ? (
+      <CryptoTableSkeleton rowsPerPage={rowsPerPage} />
+    ) : null;
+  const tableContent =
+    !error && !loading && !loadingDelay ? (
+      <CryptoTableView coins={coins} getCellClasses={getCellClasses} />
+    ) : null;
 
   return (
     <>
-      <CryptoTableControls>
-        <CryptoTableRowsPerPage
-          rowsPerPage={rowsPerPage}
-          onRowsChange={onRowsChange}
-          toggleRowsSelect={toggleRowsSelect}
-          isOpen={isRowsSelectOpen}
-          options={rowsSelectOptions}
-        />
-      </CryptoTableControls>
-      <div className="m-auto max-w-[1300px] overflow-x-auto mb-3">
-        {loading || loadingDelay ? (
-          <CryptoTableSkeleton rowsPerPage={rowsPerPage} />
-        ) : (
-          <table className="min-w-full transition-all table-auto sm:rounded-lg">
-            <thead>
-              {table.getHeaderGroups().map((headerGroup) => (
-                <tr key={headerGroup.id}>
-                  {headerGroup.headers.map((header, index) => {
-                    const cellClasses = clsx(
-                      "px-2 py-4 text-sm font-bold tracking-wider text-gray-700 bg-white cursor-pointer group",
-                      { "table-sticky-cell": header.column.id === "name" }
-                    );
-                    return (
-                      <th
-                        key={header.id}
-                        scope="col"
-                        className={cellClasses}
-                        onClick={header.column.getToggleSortingHandler()}
-                      >
-                        <div
-                          className={clsx("flex items-center gap-0.5", {
-                            "justify-end whitespace-nowrap": index > 2,
-                          })}
-                        >
-                          {header.isPlaceholder
-                            ? null
-                            : flexRender(
-                                header.column.columnDef.header,
-                                header.getContext()
-                              )}
-                          <span>
-                            {header.column.getIsSorted() ? (
-                              header.column.getIsSorted() === "desc" ? (
-                                <BiSolidDownArrow className="table-head-arrow" />
-                              ) : (
-                                <BiSolidUpArrow className="table-head-arrow" />
-                              )
-                            ) : (
-                              ""
-                            )}
-                          </span>
-                        </div>
-                      </th>
-                    );
-                  })}
-                </tr>
-              ))}
-            </thead>
-            <tbody className="bg-white ">
-              {table.getRowModel().rows.map((row) => (
-                <tr key={row.id}>
-                  {row.getVisibleCells().map((cell, index) => {
-                    return (
-                      <td
-                        key={cell.id}
-                        className={getCellClasses(cell, index)}
-                        style={{ width: cell.column.getSize() }}
-                      >
-                        {flexRender(
-                          cell.column.columnDef.cell,
-                          cell.getContext()
-                        )}
-                      </td>
-                    );
-                  })}
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        )}
-      </div>
-      <CryptoTableControls>
-        <Pagination
-          currentPage={currentPage}
-          totalCount={totalCoins}
-          pageSize={rowsPerPage}
-          onPageChange={(count: number) => setCurrentPage(count)}
-        />
-      </CryptoTableControls>
+      {errMsg ? (
+        errMsg
+      ) : (
+        <>
+          <CryptoTableControls>
+            <CryptoTableRowsPerPage
+              rowsPerPage={rowsPerPage}
+              onRowsChange={onRowsChange}
+              onToggleRowsSelect={onToggleRowsSelect}
+              isOpen={isRowsSelectOpen}
+              options={rowsSelectOptions}
+            />
+          </CryptoTableControls>
+          <div className="m-auto max-w-[1300px] overflow-x-auto mb-3">
+            {skeleton}
+            {tableContent}
+          </div>
+          <CryptoTableControls>
+            <Pagination
+              currentPage={currentPage}
+              totalCount={totalCoins}
+              pageSize={rowsPerPage}
+              onPageChange={(count: number) => setCurrentPage(count)}
+            />
+          </CryptoTableControls>
+        </>
+      )}
     </>
   );
 };
