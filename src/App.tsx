27f44313b81104b4e794fcd5c70d74c18d2659cd
Ivan Lyashuk <ivn@Ivans-MacBook-Pro.local>
2024-08-12 20:33:48 +0300

    feat: wrap CryptoTable in ErrorBoundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import CryptoTable from './components/cryptoTable/CryptoTable';
+import ErrorBoundary from './components/errorBoundary/ErrorBoundary';
 
 const App: React.FC = () => {
   
   return (
     <div className='py-10'>
-      <CryptoTable />
+      <ErrorBoundary>
+        <CryptoTable />
+      </ErrorBoundary>
     </div>
   );
 }
