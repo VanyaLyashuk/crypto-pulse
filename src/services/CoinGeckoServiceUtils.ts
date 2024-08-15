0fa96fc277c8f61a8620cc871433a780f3865881
VanyaLyashuk <ivan129522@gmail.com>
2024-08-15 21:02:03 +0300

    refactor(CoinGeckoServiceUtils): remove formatting transformations

--- src/services/CoinGeckoServiceUtils.ts
+++ src/services/CoinGeckoServiceUtils.ts
@@ -1,14 +1,11 @@
 import {
   ICoinsMarketData,
   ITransformedCoinsMarketData,
-  ITransformedPriceChange,
 } from "../models";
 
 export const transformCoinsListWithMarketData = (
   data: ICoinsMarketData[]
 ): ITransformedCoinsMarketData[] => {
-  const currency = "$";
-
   return data.map(
     ({
       id,
@@ -28,46 +25,14 @@ export const transformCoinsListWithMarketData = (
       symbol,
       name,
       image,
-      current_price: formatCurrencyValue(current_price, currency, {
-        minimumFractionDigits: 2,
-      }),
-      total_volume: formatCurrencyValue(total_volume, currency),
-      market_cap: formatCurrencyValue(market_cap, currency),
-      market_cap_rank: market_cap_rank ?? '-',
+      current_price,
+      total_volume,
+      market_cap,
+      market_cap_rank,
       sparkline_in_7d,
-      price_change_percentage_1h_in_currency: transformPriceChange(
-        price_change_percentage_1h_in_currency
-      ),
-      price_change_percentage_24h_in_currency: transformPriceChange(
-        price_change_percentage_24h_in_currency
-      ),
-      price_change_percentage_7d_in_currency: transformPriceChange(
-        price_change_percentage_7d_in_currency
-      ),
+      price_change_percentage_1h_in_currency,
+      price_change_percentage_24h_in_currency,
+      price_change_percentage_7d_in_currency,
     })
   );
 };
-
-function transformPriceChange(change: number): ITransformedPriceChange {
-  return {
-    value: formatPercentageValue(change),
-    isNegative: change < 0,
-  };
-}
-
-function formatCurrencyValue(
-  value: number,
-  currency: string,
-  options?: Intl.NumberFormatOptions
-): string {
-  if (options && value) {
-    return currency + value.toLocaleString("en-US", options);
-  } else if (value) {
-    return currency + value.toLocaleString("en-US");
-  }
-  return "-";
-}
-
-function formatPercentageValue(value: number): string {
-  return value ? `${Math.abs(value).toFixed(1)}%` : '-';
-}
