27260bd6aa83d096d13abc92960d98556c058c09
VanyaLyashuk <ivan129522@gmail.com>
2024-08-15 20:52:19 +0300

    refactor: update TransformedCoinsMarketData interface to use numbers instead of strings

--- src/models/TransformedCoinsMarketData.interface.ts
+++ src/models/TransformedCoinsMarketData.interface.ts
@@ -1,18 +1,16 @@
-import { ITransformedPriceChange } from "./TransformedPriceChange.interface";
-
 export interface ITransformedCoinsMarketData {
   id: string;
   symbol: string;
   name: string;
   image: string;
-  current_price: string;
-  total_volume: string;
-  market_cap: string;
-  market_cap_rank: number | '-';
+  current_price: number;
+  total_volume: number;
+  market_cap: number;
+  market_cap_rank: number;
   sparkline_in_7d: {
     price: number[]
   }
-  price_change_percentage_1h_in_currency: ITransformedPriceChange;
-  price_change_percentage_24h_in_currency: ITransformedPriceChange;
-  price_change_percentage_7d_in_currency: ITransformedPriceChange;
+  price_change_percentage_1h_in_currency: number;
+  price_change_percentage_24h_in_currency: number;
+  price_change_percentage_7d_in_currency: number;
 }
