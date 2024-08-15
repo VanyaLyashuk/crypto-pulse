6e5e9619f18f3591ceeaf2e07c0ff71e4fff2bb2
VanyaLyashuk <ivan129522@gmail.com>
2024-08-15 21:07:10 +0300

    refactor(PriceChangeCell): update component to handle new conditions

--- src/components/cryptoTable/PriceChangeCell.tsx
+++ src/components/cryptoTable/PriceChangeCell.tsx
@@ -1,27 +1,20 @@
 import clsx from "clsx";
-import React from "react";
-
 import { ITransformedPriceChange } from "../../models";
+import { formatPercentageValue } from "../../utils/CryptoTableUtils";
 import ArrowIcon from "../UI/ArrowIcon";
 
-const PriceChangeCell: React.FC<ITransformedPriceChange> = ({
-  value,
-  isNegative,
-}) => {
+const PriceChangeCell: React.FC<ITransformedPriceChange> = ({ value }) => {
+  const isNegative: boolean = value < 0;
   const cellClasses = clsx("flex items-center justify-end gap-0.5", {
-    "text-red-600": isNegative && value !== '-',
-    "text-green-500": !isNegative && value !== '-',
-    "text-gray-700": value === '-' && isNegative,
+    "text-red-600": value && isNegative,
+    "text-green-500": value && !isNegative,
+    "text-gray-700": !value,
   });
 
   return (
-    <div className={cellClasses}>{
-      value === '-' ? '-' : 
-      (<>
-        <ArrowIcon isNegative={isNegative} />
-        <span>{value}</span>
-      </>)
-    }
+    <div className={cellClasses}>
+      {value ? <ArrowIcon isNegative={isNegative} /> : null}
+      <span>{formatPercentageValue(value)}</span>
     </div>
   );
 };
