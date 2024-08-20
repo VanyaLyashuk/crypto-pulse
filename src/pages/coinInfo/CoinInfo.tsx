5e088c16dc5a4b46138c3a9b4934109bd61d3294
VanyaLyashuk <ivan129522@gmail.com>
2024-08-20 11:13:58 +0300

    feat(CoinInfo): add basic styles and functionality

--- src/pages/coinInfo/CoinInfo.tsx
+++ src/pages/coinInfo/CoinInfo.tsx
@@ -1,14 +1,33 @@
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { ICoinInfoProps } from "../../models";
 
 const CoinInfo: React.FC<ICoinInfoProps> = ({ id }) => {
   const navigate = useNavigate();
+
   const closeModal = () => navigate(-1);
 
+  useEffect(() => {
+    document.body.classList.add("overflow-hidden");
+
+    return () => document.body.classList.remove("overflow-hidden");
+  }, []);
+
+  const handleInnerClick = (event: React.MouseEvent) => {
+    event.stopPropagation();
+  };
+
   return (
-    <div>
-      <button onClick={closeModal}>Close Modal</button>
-      <div>Coin info for {id}</div>
+    <div
+      onClick={closeModal}
+      className="fixed inset-0 z-50 flex items-end justify-end bg-black bg-opacity-50 sm:p-6 sm:items-center"
+    >
+      <div
+        onClick={handleInnerClick}
+        className="w-full px-2 py-6 bg-white rounded-tl-xl rounded-tr-xl min-h-50vh sm:rounded-xl sm:max-w-[1024px] sm:m-auto"
+      >
+        Coin info for {id}
+      </div>
     </div>
   );
 };
