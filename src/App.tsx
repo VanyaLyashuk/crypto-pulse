bf8d2438b62fef97e64bb7a21906e5cfd3b2af0c
VanyaLyashuk <ivan129522@gmail.com>
2024-08-20 09:52:21 +0300

    feat: add basic routing and state for setting coin ID, passed function through Home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
-import React from "react";
+import { useCallback, useState } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
+import CoinInfo from "./pages/coinInfo/CoinInfo";
 import Home from "./pages/home/Home";
 
 const App: React.FC = () => {
+  const [coinId, setCoinId] = useState<string>("");
+
+  const handleSetCoinId = useCallback((id: string) => {
+    setCoinId(id);
+  }, [])
+  
+  const location = useLocation();
+  const state = location.state as { backgroundLocation?: Location };
+
   return (
     <div className="py-10">
-      <Home />
+      <>
+        <Routes location={state?.backgroundLocation || location}>
+          <Route path="/" element={<Home handleSetCoinId={handleSetCoinId} />} />
+          <Route path="/coin/:id" element={<CoinInfo id={coinId} />} />
+        </Routes>
+        {state?.backgroundLocation && (
+          <Routes>
+            <Route path="/coin/:id" element={<CoinInfo id={coinId} />} />
+          </Routes>
+        )}
+      </>
     </div>
   );
 };
