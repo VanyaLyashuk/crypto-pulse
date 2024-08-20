f70f2e62f36eb984724e6dfd115ee75cb692938f
VanyaLyashuk <ivan129522@gmail.com>
2024-08-20 10:04:27 +0300

    feat(CoinInfo): add closeModal function

--- src/pages/coinInfo/CoinInfo.tsx
+++ src/pages/coinInfo/CoinInfo.tsx
@@ -1,7 +1,16 @@
+import { useNavigate } from "react-router-dom";
 import { ICoinInfoProps } from "../../models";
 
 const CoinInfo: React.FC<ICoinInfoProps> = ({ id }) => {
-  return <div>Coin {id}</div>;
+  const navigate = useNavigate();
+  const closeModal = () => navigate(-1);
+
+  return (
+    <div>
+      <button onClick={closeModal}>Close Modal</button>
+      <div>Coin info for {id}</div>
+    </div>
+  );
 };
 
 export default CoinInfo;
