0912eccaa1e5a610ac1949050c9142163da996dc
VanyaLyashuk <ivan129522@gmail.com>
2024-08-21 15:57:56 +0300

    refactor: change index file with new file names and imports

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,22 +1,23 @@
+import { IArrowIconProps } from "./componentProps/ArrowIconProps.interface";
 import { ICoinInfoProps } from "./componentProps/CoinInfoProps.interface";
 import { ICryptoTableRowsPerPageProps } from "./componentProps/CryptoTableRowsPerPageProps.interface";
 import { ICryptoTableSkeletonProps } from "./componentProps/CryptoTableSkeletonProps.interface";
 import { ICryptoTableViewProps } from "./componentProps/CryptoTableViewProps.interface";
 import { IErrorBoundaryProps } from "./componentProps/ErrorBoundaryProps.interface";
 import { IErrorMessageProps } from "./componentProps/ErrorMessageProps.interface";
+import { IPriceChangeIndicatorProps } from "./componentProps/PriceChangeIndicatorProps.interface";
 import { ISparklineChartProps } from "./componentProps/SparklineChartProps.interface";
 import { ICoinsListData } from "./dataTypes/CoinsListData.interface";
 import { ICoinsMarketData } from "./dataTypes/CoinsMarketData.interface";
 import { TCryptoTableCellContext } from "./dataTypes/CryptoTableCellContext.type";
 import { TCryptoTableCurrency } from "./dataTypes/CryptoTableCurrency.type";
 import { ITransformedCoinsMarketData } from "./dataTypes/TransformedCoinsMarketData.interface";
-import { ITransformedPriceChange } from "./dataTypes/TransformedPriceChange.interface";
 import { IPaginationProps } from "./pagination/Pagination.interface";
 import { IPaginationComponentProps } from "./pagination/PaginationComponentProps.interface";
 import { IErrorBoundaryState } from "./stateAndErrors/ErrorBoundaryState.interface";
 
 export type {
-    ICoinInfoProps, ICoinsListData,
+    IArrowIconProps, ICoinInfoProps, ICoinsListData,
     ICoinsMarketData,
     ICryptoTableRowsPerPageProps,
     ICryptoTableSkeletonProps,
@@ -25,11 +26,8 @@ export type {
     IErrorBoundaryState,
     IErrorMessageProps,
     IPaginationComponentProps,
-    IPaginationProps,
-    ISparklineChartProps,
-    ITransformedCoinsMarketData,
-    ITransformedPriceChange,
-    TCryptoTableCellContext,
+    IPaginationProps, IPriceChangeIndicatorProps, ISparklineChartProps,
+    ITransformedCoinsMarketData, TCryptoTableCellContext,
     TCryptoTableCurrency
 };
 
