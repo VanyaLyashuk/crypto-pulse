b25397988053bdefccdddef0c75335fa3c6f3796
VanyaLyashuk <ivan129522@gmail.com>
2024-08-24 21:59:14 +0300

    feat: add CoinInfoTable component

---
+++ src/components/coinInfoTable/CoinInfoTable.tsx
@@ -0,0 +1,82 @@
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import { useMemo } from "react";
+import PriceChangeIndicator from "../priceChangeIndicator/PriceChangeIndicator";
+
+interface ICoinInfoTableItem {
+  label: string;
+  value: number;
+}
+
+interface ICoinInfoTableProps {
+  data: ICoinInfoTableItem[];
+}
+
+const CoinInfoTable: React.FC<ICoinInfoTableProps> = ({ data }) => {
+  const columns = useMemo<ColumnDef<Record<string, number>>[]>(
+    () =>
+      data.map((item) => ({
+        header: item.label,
+        accessorKey: item.label,
+        cell: (info) => (
+          <PriceChangeIndicator
+            arrowSize="w-2 h-2"
+            alignment="center"
+            className="text-sm"
+            value={info.getValue<number>()}
+          />
+        ),
+      })),
+    [data]
+  );
+
+  const tableData = useMemo(() => {
+    const rowData: Record<string, number> = {};
+    data.forEach((item) => {
+      rowData[item.label] = item.value;
+    });
+    return [rowData];
+  }, [data]);
+
+  const table = useReactTable({
+    data: tableData,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <table className="w-full overflow-hidden text-center">
+      <thead className="text-sm bg-gray-100">
+        {table.getHeaderGroups().map((headerGroup) => (
+          <tr key={headerGroup.id}>
+            {headerGroup.headers.map((header) => (
+              <th key={header.id} className="p-2 border-r last:border-r-0">
+                {flexRender(
+                  header.column.columnDef.header,
+                  header.getContext()
+                )}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody>
+        {table.getRowModel().rows.map((row) => (
+          <tr key={row.id}>
+            {row.getVisibleCells().map((cell) => (
+              <td key={cell.id} className="p-2 border-r last:border-r-0">
+                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default CoinInfoTable;
