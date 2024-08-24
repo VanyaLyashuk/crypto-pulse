000fc11b7f25d3b292df870c822676500726a562
VanyaLyashuk <ivan129522@gmail.com>
2024-08-24 21:55:16 +0300

    refactor(PriceChangeIndicator): add alignment prop and styles for better reusability

--- src/components/priceChangeIndicator/PriceChangeIndicator.tsx
+++ src/components/priceChangeIndicator/PriceChangeIndicator.tsx
@@ -7,12 +7,15 @@ const PriceChangeIndicator: React.FC<IPriceChangeIndicatorProps> = ({
   arrowSize,
   className,
   value,
+  alignment = 'end'
 }) => {
   const isNegative: boolean = value < 0;
-  const containerClasses = clsx("flex items-center justify-end gap-0.5", {
+  const containerClasses = clsx("flex items-center gap-0.5", {
     "text-red-600": value && isNegative,
     "text-green-500": value && !isNegative,
     "text-gray-700": !value,
+    "justify-end": alignment === 'end',
+    "justify-center": alignment === 'center',
   });
 
   return (
