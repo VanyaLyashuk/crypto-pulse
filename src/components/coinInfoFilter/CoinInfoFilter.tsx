02828e6825215ef27d7c7347004398c038e6b512
VanyaLyashuk <ivan129522@gmail.com>
2024-08-26 10:22:20 +0300

    refactor(CoinInfoFilter): pass DatePicker props through component

--- src/components/coinInfoFilter/CoinInfoFilter.tsx
+++ src/components/coinInfoFilter/CoinInfoFilter.tsx
@@ -1,26 +1,42 @@
 import clsx from "clsx";
 import { BsCalendar2 } from "react-icons/bs";
 import { ICoinInfoFilterProps } from "../../models";
+import CoinInfoDatepicker from "../coinInfoDatePicker/CoinInfoDatePicker";
 
 const CoinInfoFilter: React.FC<ICoinInfoFilterProps> = ({
   filterOptions,
   activeFilter,
   onFilterChange,
+  startDate,
+  endDate,
+  isOpen,
+  handleDateChange,
+  toggleDatepicker,
 }) => {
   const buttons = filterOptions.map((filter) => {
     const btnClasses = clsx("px-2 py-1 text-sm font-medium", {
       "bg-white rounded-md shadow-sm": activeFilter === filter,
-      "px-1 mx-1": activeFilter === "date range",
     });
 
     return filter === "date range" ? (
-      <button
-        key={filter}
-        className={btnClasses}
-        onClick={() => onFilterChange(filter)}
-      >
-        <BsCalendar2 />
-      </button>
+      <div className="relative inline-block" key={filter}>
+        <button
+          className={btnClasses}
+          onClick={() => {
+            onFilterChange(filter);
+            toggleDatepicker ? toggleDatepicker() : null;
+          }}
+        >
+          <BsCalendar2 />
+        </button>
+        {isOpen && (
+          <CoinInfoDatepicker
+            handleDateChange={handleDateChange}
+            startDate={startDate}
+            endDate={endDate}
+          />
+        )}
+      </div>
     ) : (
       <button
         key={filter}
--- src/models/componentProps/CoinInfoFilterProps.interface.ts
+++ src/models/componentProps/CoinInfoFilterProps.interface.ts
@@ -1,8 +1,14 @@
 import { TCoinInfoMetric } from "../dataTypes/CoinInfoMetric.type";
 import { TCoinInfoTimeRange } from "../dataTypes/CoinInfoTimeRange.type";
+import { TDateChangeHandler } from "../dataTypes/DateChangeHandler.type";
 
 export interface ICoinInfoFilterProps {
   filterOptions: TCoinInfoMetric[] | TCoinInfoTimeRange[];
   activeFilter: string;
   onFilterChange: (filter: TCoinInfoMetric | TCoinInfoTimeRange) => void;
+  startDate?: Date | null;
+  endDate?: Date | null;
+  isOpen?: boolean;
+  handleDateChange?: TDateChangeHandler;
+  toggleDatepicker?: () => void;
 }
