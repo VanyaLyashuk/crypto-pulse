4deb0bcfefc1e0eb0f5983924d6c0bc077f1c4fe
VanyaLyashuk <ivan129522@gmail.com>
2024-09-02 21:05:00 +0300

    feat: create table view store

---
+++ src/store/tableView.store.ts
@@ -0,0 +1,17 @@
+import { create } from "zustand";
+import { devtools } from "zustand/middleware";
+import { ITableViewStore } from "../models";
+
+const useTableViewStore = create<ITableViewStore>()(
+  devtools(
+    (set) => ({
+      rowsPerPage: 10,
+      setRowsPerPage: (rows) => set({ rowsPerPage: rows }),
+      isRowsSelectOpen: false,
+      setIsRowsSelectOpen: (isOpen) => set({ isRowsSelectOpen: isOpen }),
+    }),
+    { name: "tableView" }
+  )
+);
+
+export default useTableViewStore;
