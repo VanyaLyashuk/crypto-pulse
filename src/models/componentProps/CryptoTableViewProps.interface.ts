b1b7674a290676783207786be9a6cbb63502d4c5
VanyaLyashuk <ivan129522@gmail.com>
2024-09-03 11:02:48 +0300

    refactor(CryptoTableViewProps): remove unnecessary props

--- src/models/componentProps/CryptoTableViewProps.interface.ts
+++ src/models/componentProps/CryptoTableViewProps.interface.ts
@@ -1,13 +1,5 @@
-import { Cell } from "@tanstack/react-table";
 import { TCryptoTableCurrency } from "../dataTypes/CryptoTableCurrency.type";
-import { ITransformedCoinsMarketData } from "../dataTypes/TransformedCoinsMarketData.interface";
 
 export interface ICryptoTableViewProps {
-  coins: ITransformedCoinsMarketData[];
-  getCellClasses: (
-    cell: Cell<ITransformedCoinsMarketData, unknown>,
-    index: number
-  ) => string;
   currency: TCryptoTableCurrency;
-  handleSetCoinId: (id: string) => void;
 }
