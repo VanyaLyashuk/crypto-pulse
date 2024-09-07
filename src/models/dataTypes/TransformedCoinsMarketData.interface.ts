f58fa7b6a231ba586697344bb4ec35f4ea5c3b79
VanyaLyashuk <ivan129522@gmail.com>
2024-09-07 11:02:18 +0300

    refactor: extend TransformedCoinsMarketData interface with CoinStatisticsData and HistoricalPriceData interfaces. Add 14d, 30d, 1y price change percentages

--- src/models/dataTypes/TransformedCoinsMarketData.interface.ts
+++ src/models/dataTypes/TransformedCoinsMarketData.interface.ts
@@ -1,16 +1,25 @@
-export interface ITransformedCoinsMarketData {
+import { ICoinStatisticsData } from "./CoinStatisticsData.interface";
+import { IHistoricalPriceData } from "./HistoricalPriceData.interface";
+
+export interface ITransformedCoinsMarketData
+  extends ICoinStatisticsData,
+    IHistoricalPriceData {
   id: string;
   symbol: string;
   name: string;
   image: string;
   current_price: number;
-  total_volume: number;
+  current_price_formatted: string;
   market_cap: number;
   market_cap_rank: number;
+  total_volume: number;
   sparkline_in_7d: {
-    price: number[]
-  }
+    price: number[];
+  };
   price_change_percentage_1h_in_currency: number;
   price_change_percentage_24h_in_currency: number;
   price_change_percentage_7d_in_currency: number;
+  price_change_percentage_14d_in_currency: number;
+  price_change_percentage_30d_in_currency: number;
+  price_change_percentage_1y_in_currency: number;
 }
