d84074272346c75aabaa8d9745f722ca6a476fe6
VanyaLyashuk <ivan129522@gmail.com>
2024-09-07 10:57:41 +0300

    refactor: use LabeledValueExtended interface in HistoricalPriceData interface

--- src/models/dataTypes/HistoricalPriceData.interface.ts
+++ src/models/dataTypes/HistoricalPriceData.interface.ts
@@ -1,18 +1,9 @@
 import { ILabeledValue } from "./LabeledValue.interface";
+import { ILabeledValueExtended } from "./LabeledValueExtended.interface";
 
 export interface IHistoricalPriceData {
-  "24h_range": ILabeledValue;
-  "7d_range": ILabeledValue;
-  "all_time_high": {
-    label: string;
-    price: string;
-    price_change_percentage: number;
-    date: string;
-  }
-  "all_time_low": {
-    label: string;
-    price: string;
-    price_change_percentage: number;
-    date: string;
-  }
+  range_24h: ILabeledValue;
+  range_7d: ILabeledValue;
+  ath: ILabeledValueExtended;
+  atl: ILabeledValueExtended;
 }
