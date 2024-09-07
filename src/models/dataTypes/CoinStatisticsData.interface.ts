60615fca151b314fd9acfb5ed00d2e21d194b429
VanyaLyashuk <ivan129522@gmail.com>
2024-09-07 10:56:16 +0300

    refactor: rename market_cap to market_cap_formatted ad 24h_trading_volume to trading_volume_24h in CoinStatisticsData interface

--- src/models/dataTypes/CoinStatisticsData.interface.ts
+++ src/models/dataTypes/CoinStatisticsData.interface.ts
@@ -1,9 +1,9 @@
 import { ILabeledValue } from "./LabeledValue.interface";
 
 export interface ICoinStatisticsData {
-  market_cap: ILabeledValue;
+  market_cap_formatted: ILabeledValue;
   fully_diluted_valuation: ILabeledValue;
-  "24h_trading_volume": ILabeledValue;
+  trading_volume_24h: ILabeledValue;
   circulating_supply: ILabeledValue;
   total_supply: ILabeledValue;
   max_supply: ILabeledValue;
