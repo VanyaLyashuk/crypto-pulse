8a58dc5df927b5891897d1b2d98f3906f0411683
VanyaLyashuk <ivan129522@gmail.com>
2024-09-07 13:32:57 +0300

    fix(CoinGeckoServiceUtils): update data transformation to match new interface format

--- src/utils/CoinGeckoServiceUtils.ts
+++ src/utils/CoinGeckoServiceUtils.ts
@@ -1,7 +1,5 @@
-import {
-  ICoinsMarketData,
-  ITransformedCoinsMarketData,
-} from "../models";
+import { ICoinsMarketData, ITransformedCoinsMarketData } from "../models";
+import { formatCurrencyValue, getMinMaxValue } from "./CryptoTableUtils";
 
 export const transformCoinsListWithMarketData = (
   data: ICoinsMarketData[]
@@ -13,26 +11,95 @@ export const transformCoinsListWithMarketData = (
       name,
       image,
       current_price,
-      total_volume,
       market_cap,
       market_cap_rank,
+      fully_diluted_valuation,
+      total_volume,
+      high_24h,
+      low_24h,
+      circulating_supply,
+      total_supply,
+      max_supply,
+      ath,
+      ath_change_percentage,
+      ath_date,
+      atl,
+      atl_change_percentage,
+      atl_date,
       sparkline_in_7d,
       price_change_percentage_1h_in_currency,
       price_change_percentage_24h_in_currency,
       price_change_percentage_7d_in_currency,
+      price_change_percentage_14d_in_currency,
+      price_change_percentage_30d_in_currency,
+      price_change_percentage_1y_in_currency,
     }) => ({
       id,
       symbol,
       name,
       image,
       current_price,
-      total_volume,
+      current_price_formatted: formatCurrencyValue(current_price, "$"),
       market_cap,
+      market_cap_formatted: {
+        label: "Market Cap",
+        price: formatCurrencyValue(market_cap, "$"),
+      },
       market_cap_rank,
+      fully_diluted_valuation: {
+        label: "Fully Diluted Valuation",
+        price: formatCurrencyValue(fully_diluted_valuation, "$"),
+      },
+      total_volume,
+      trading_volume_24h: {
+        label: "24 Hour Trading Vol",
+        price: formatCurrencyValue(total_volume, "$"),
+      },
+      circulating_supply: {
+        label: "Circulating Supply",
+        price: formatCurrencyValue(circulating_supply),
+      },
+      total_supply: {
+        label: "Total Supply",
+        price: formatCurrencyValue(total_supply),
+      },
+      max_supply: {
+        label: "Max Supply",
+        price: formatCurrencyValue(max_supply),
+      },
+      range_24h: {
+        label: "24h Range",
+        price:
+          formatCurrencyValue(low_24h, "$") +
+          "-" +
+          formatCurrencyValue(high_24h, "$"),
+      },
+      range_7d: {
+        label: "7d Range",
+        price:
+          formatCurrencyValue(getMinMaxValue(sparkline_in_7d.price).min, "$") +
+          "-" +
+          formatCurrencyValue(getMinMaxValue(sparkline_in_7d.price).max, "$"),
+      },
+      ath: {
+        label: "All-Time High",
+        price: formatCurrencyValue(ath, "$"),
+        percentage: ath_change_percentage,
+        date: ath_date,
+      },
+      atl: {
+        label: "All-Time Low",
+        price: formatCurrencyValue(atl, "$"),
+        percentage: atl_change_percentage,
+        date: atl_date,
+      },
       sparkline_in_7d,
       price_change_percentage_1h_in_currency,
       price_change_percentage_24h_in_currency,
       price_change_percentage_7d_in_currency,
+      price_change_percentage_14d_in_currency,
+      price_change_percentage_30d_in_currency,
+      price_change_percentage_1y_in_currency,
     })
   );
 };
