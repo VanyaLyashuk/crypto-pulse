8c2282eff23353d35141b4133ecc672e179b8b64
VanyaLyashuk <ivan129522@gmail.com>
2024-09-07 10:49:51 +0300

    feat: add 14d, 30d, 1y price change percentages to CoinsMarketData interface

--- src/models/dataTypes/CoinsMarketData.interface.ts
+++ src/models/dataTypes/CoinsMarketData.interface.ts
@@ -31,4 +31,7 @@ export interface ICoinsMarketData {
   price_change_percentage_1h_in_currency: number;
   price_change_percentage_24h_in_currency: number;
   price_change_percentage_7d_in_currency: number;
+  price_change_percentage_14d_in_currency: number;
+  price_change_percentage_30d_in_currency: number;
+  price_change_percentage_1y_in_currency: number;
 }
