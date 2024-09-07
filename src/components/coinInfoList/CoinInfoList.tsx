ef51ccf1736a0a84766f676103d3b72a73c16ffd
VanyaLyashuk <ivan129522@gmail.com>
2024-09-07 13:39:45 +0300

    refactor(CoinInfoList): pass name props to title and change render condition

--- src/components/coinInfoList/CoinInfoList.tsx
+++ src/components/coinInfoList/CoinInfoList.tsx
@@ -8,38 +8,38 @@ const CoinInfoList: React.FC<ICoinInfoListProps> = ({ name, title, data }) => {
     "mb-6 lg:mb-0 lg:col-span-4": title === "Statistics",
   });
 
-  const liClasses =
-    "flex items-start justify-between py-3 font-medium border-b last:border-b-0 last:pb-0";
+  const liClasses = "flex items-start justify-between py-3 font-medium border-b last:border-b-0 last:pb-0";
 
   const listItems = data
-    ? Object.entries(data).map(([key, data]) => {
-        return key === "all_time_high" || key === "all_time_low" ? (
-          <li key={key} className={liClasses}>
-            <div className="flex items-center gap-1">
-              <h5 className="text-gray-500">{data.label}</h5>{" "}
-              <PiInfo className="relative top-[1px]" />
-            </div>
-            <div className="flex items-center justify-end font-medium gap-x-2">
-              <h4>{data.price}</h4>
-              <PriceChangeIndicator
-                arrowSize="w-2 h-2"
-                value={data.price_change_percentage}
-              />
-            </div>
-          </li>
-        ) : (
-          <li key={key} className={liClasses}>
-            <h5 className="text-gray-500">{data.label}</h5>{" "}
-            <div>{data.price}</div>
-          </li>
-        );
+    ? Object.entries(data).map(([key, value]) => {
+        if ('percentage' in value) {
+          return (
+            <li key={key} className={liClasses}>
+              <div className="flex items-center gap-1">
+                <h5 className="text-gray-500">{value.label}</h5>{" "}
+                <PiInfo className="relative top-[1px]" />
+              </div>
+              <div className="flex items-center justify-end font-medium gap-x-2">
+                <h4>{value.price}</h4>
+                <PriceChangeIndicator arrowSize="w-2 h-2" value={value.percentage} />
+              </div>
+            </li>
+          );
+        } else {
+          return (
+            <li key={key} className={liClasses}>
+              <h5 className="text-gray-500">{value.label}</h5>{" "}
+              <div>{value.price}</div>
+            </li>
+          );
+        }
       })
     : null;
 
   return (
     <div className={wrapperClasses}>
       <h5 className="text-2xl font-bold ">
-        {name} {title}
+        <span className="uppercase">{name}</span> {title}
       </h5>
       <ul>{listItems}</ul>
     </div>
