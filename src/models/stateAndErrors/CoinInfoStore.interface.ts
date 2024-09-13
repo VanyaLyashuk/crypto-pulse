24d2fc858fae5cbf4f777c1f3ad48a33505c59b9
VanyaLyashuk <ivan129522@gmail.com>
2024-09-13 18:22:26 +0300

    feat: add selectedCoinId and setSelectedCoinId to CoinInfoStore interface

--- src/models/stateAndErrors/CoinInfoStore.interface.ts
+++ src/models/stateAndErrors/CoinInfoStore.interface.ts
@@ -1,6 +1,8 @@
 import { ITransformedCoinsMarketData } from "../dataTypes/TransformedCoinsMarketData.interface";
 
 export interface ICoinInfoStore {
+  selectedCoinId: string;
+  setSelectedCoinId: (id: string) => void;
   selectedCoin: ITransformedCoinsMarketData;
   setSelectedCoin: (id: string) => void;
 }
