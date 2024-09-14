f12737500efd4b8141d7584e5382e5983d935305
VanyaLyashuk <ivan129522@gmail.com>
2024-09-14 12:39:09 +0300

    refactor: remove unnecessary coin state from CoinInfoStore interface

--- src/models/stateAndErrors/CoinInfoStore.interface.ts
+++ src/models/stateAndErrors/CoinInfoStore.interface.ts
@@ -1,8 +1,4 @@
-import { ITransformedCoinsMarketData } from "../dataTypes/TransformedCoinsMarketData.interface";
-
 export interface ICoinInfoStore {
   selectedCoinId: string;
   setSelectedCoinId: (id: string) => void;
-  selectedCoin: ITransformedCoinsMarketData;
-  setSelectedCoin: (id: string) => void;
 }
