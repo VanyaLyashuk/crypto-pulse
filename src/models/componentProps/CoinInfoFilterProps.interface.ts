ae5e56a30463c1d39a2b0849640fad3bec40d084
VanyaLyashuk <ivan129522@gmail.com>
2024-09-14 17:13:54 +0300

    refactor: remove unused props from coin info filter props interface

--- src/models/componentProps/CoinInfoFilterProps.interface.ts
+++ src/models/componentProps/CoinInfoFilterProps.interface.ts
@@ -1,11 +1,7 @@
 import { TCoinInfoMetric } from "../dataTypes/CoinInfoMetric.type";
 import { TCoinInfoTimeRange } from "../dataTypes/CoinInfoTimeRange.type";
-import { TDateChangeHandler } from "../dataTypes/DateChangeHandler.type";
 
 export interface ICoinInfoFilterProps {
   filterOptions: TCoinInfoMetric[] | TCoinInfoTimeRange[];
   activeFilter: string;
-  startDate?: Date | null;
-  endDate?: Date | null;
-  handleDateChange?: TDateChangeHandler;
 }
