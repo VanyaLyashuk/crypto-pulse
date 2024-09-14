243734b8bfc05804349023e81973da29bc0c7de1
VanyaLyashuk <ivan129522@gmail.com>
2024-09-14 15:13:19 +0300

    feat: add selectedTimeRange and set method to coin info store interface

--- src/models/stateAndErrors/CoinInfoStore.interface.ts
+++ src/models/stateAndErrors/CoinInfoStore.interface.ts
@@ -1,8 +1,11 @@
 import { TCoinInfoMetric } from "../dataTypes/CoinInfoMetric.type";
+import { TCoinInfoTimeRange } from "../dataTypes/CoinInfoTimeRange.type";
 
 export interface ICoinInfoStore {
   selectedCoinId: string;
   setSelectedCoinId: (id: string) => void;
   selectedMetric: TCoinInfoMetric;
   setSelectedMetric: (value: TCoinInfoMetric) => void;
+  selectedTimeRange: TCoinInfoTimeRange;
+  setSelectedTimeRange: (value: TCoinInfoTimeRange) => void;
 }
