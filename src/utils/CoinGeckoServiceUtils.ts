c38911258b568ca488ab4e8ceeb231d27815f9c5
VanyaLyashuk <ivan129522@gmail.com>
2024-09-14 17:41:32 +0300

    refactor(CoinGeckoServiceUtils): update transform function to return data in new coin_statistics and coin_historical_price properties

--- src/utils/CoinGeckoServiceUtils.ts
+++ src/utils/CoinGeckoServiceUtils.ts
@@ -53,58 +53,8 @@ export const transformCoinsListWithMarketData = (
       current_price,
       current_price_formatted: formatCurrencyValue(current_price, "$"),
       market_cap,
-      market_cap_formatted: {
-        label: "Market Cap",
-        price: formatCurrencyValue(market_cap, "$"),
-      },
       market_cap_rank,
-      fully_diluted_valuation: {
-        label: "Fully Diluted Valuation",
-        price: formatCurrencyValue(fully_diluted_valuation, "$"),
-      },
       total_volume,
-      trading_volume_24h: {
-        label: "24 Hour Trading Vol",
-        price: formatCurrencyValue(total_volume, "$"),
-      },
-      circulating_supply: {
-        label: "Circulating Supply",
-        price: formatCurrencyValue(circulating_supply),
-      },
-      total_supply: {
-        label: "Total Supply",
-        price: formatCurrencyValue(total_supply),
-      },
-      max_supply: {
-        label: "Max Supply",
-        price: formatCurrencyValue(max_supply),
-      },
-      range_24h: {
-        label: "24h Range",
-        price:
-          formatCurrencyValue(low_24h, "$") +
-          "-" +
-          formatCurrencyValue(high_24h, "$"),
-      },
-      range_7d: {
-        label: "7d Range",
-        price:
-          formatCurrencyValue(getMinMaxValue(sparkline_in_7d.price).min, "$") +
-          "-" +
-          formatCurrencyValue(getMinMaxValue(sparkline_in_7d.price).max, "$"),
-      },
-      ath: {
-        label: "All-Time High",
-        price: formatCurrencyValue(ath, "$"),
-        percentage: ath_change_percentage,
-        date: formatDate(ath_date),
-      },
-      atl: {
-        label: "All-Time Low",
-        price: formatCurrencyValue(atl, "$"),
-        percentage: atl_change_percentage,
-        date: formatDate(atl_date),
-      },
       sparkline_in_7d,
       price_change_percentage_1h_in_currency,
       price_change_percentage_24h_in_currency,
@@ -112,6 +62,63 @@ export const transformCoinsListWithMarketData = (
       price_change_percentage_14d_in_currency,
       price_change_percentage_30d_in_currency,
       price_change_percentage_1y_in_currency,
+      coin_statistics: {
+        market_cap_formatted: {
+          label: "Market Cap",
+          price: formatCurrencyValue(market_cap, "$"),
+        },
+        fully_diluted_valuation: {
+          label: "Fully Diluted Valuation",
+          price: formatCurrencyValue(fully_diluted_valuation, "$"),
+        },
+        trading_volume_24h: {
+          label: "24 Hour Trading Vol",
+          price: formatCurrencyValue(total_volume, "$"),
+        },
+        circulating_supply: {
+          label: "Circulating Supply",
+          price: formatCurrencyValue(circulating_supply),
+        },
+        total_supply: {
+          label: "Total Supply",
+          price: formatCurrencyValue(total_supply),
+        },
+        max_supply: {
+          label: "Max Supply",
+          price: formatCurrencyValue(max_supply),
+        },
+      },
+      coin_historical_price: {
+        range_24h: {
+          label: "24h Range",
+          price:
+            formatCurrencyValue(low_24h, "$") +
+            "-" +
+            formatCurrencyValue(high_24h, "$"),
+        },
+        range_7d: {
+          label: "7d Range",
+          price:
+            formatCurrencyValue(
+              getMinMaxValue(sparkline_in_7d.price).min,
+              "$"
+            ) +
+            "-" +
+            formatCurrencyValue(getMinMaxValue(sparkline_in_7d.price).max, "$"),
+        },
+        ath: {
+          label: "All-Time High",
+          price: formatCurrencyValue(ath, "$"),
+          percentage: ath_change_percentage,
+          date: formatDate(ath_date),
+        },
+        atl: {
+          label: "All-Time Low",
+          price: formatCurrencyValue(atl, "$"),
+          percentage: atl_change_percentage,
+          date: formatDate(atl_date),
+        },
+      },
     })
   );
 };
