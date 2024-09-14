7fb642efd7c0a67fd41ef75a92fb345b86c728ae
VanyaLyashuk <ivan129522@gmail.com>
2024-09-14 14:02:52 +0300

    feat: add selectedMetric and set method to coin info store interface

--- src/models/stateAndErrors/CoinInfoStore.interface.ts
+++ src/models/stateAndErrors/CoinInfoStore.interface.ts
@@ -1,4 +1,8 @@
+import { TCoinInfoMetric } from "../dataTypes/CoinInfoMetric.type";
+
 export interface ICoinInfoStore {
   selectedCoinId: string;
   setSelectedCoinId: (id: string) => void;
+  selectedMetric: TCoinInfoMetric;
+  setSelectedMetric: (value: TCoinInfoMetric) => void;
 }
