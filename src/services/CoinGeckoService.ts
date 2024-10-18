eefd6f0048731872cf6aed5ad1e6e70a65857ba2
VanyaLyashuk <ivan129522@gmail.com>
2024-10-18 16:44:10 +0300

    refactor(CoinGeckoService): use new useHttp hook instead of class method and rewrite service from class to hook

--- src/services/CoinGeckoService.ts
+++ src/services/CoinGeckoService.ts
@@ -1,3 +1,4 @@
+import { useHttp } from "../hooks/useHttp";
 import {
   ICoinHistoricalChartDataById,
   ICoinsListData,
@@ -9,61 +10,52 @@ import {
   transformCoinsListWithMarketData,
 } from "../utils/CoinGeckoServiceUtils";
 
-class CoinGeckoService {
-  private readonly _apiKey: string = import.meta.env.VITE_COINGECKO_API_KEY;
-  private readonly _apiBase: string = "https://api.coingecko.com/api/v3/";
-  private readonly _vsCurrency: string = "usd";
-  private readonly _perPage: number = 30;
-  private readonly _page: number = 1;
+const useCoinGeckoService = () => {
+  const { loading, request, error } = useHttp();
 
-  private async getResource<T>(url: string): Promise<T> {
-    const options = {
-      method: "GET",
-      headers: {
-        accept: "application/json",
-      },
-    };
+  const _apiKey: string = import.meta.env.VITE_COINGECKO_API_KEY;
+  const _apiBase: string = "https://api.coingecko.com/api/v3/";
+  const _vsCurrency: string = "usd";
 
-    const response = await fetch(url, options);
-
-    if (!response.ok) {
-      throw new Error(`Could not fetch ${url}, status: ${response.status}`);
-    }
-
-    return (await response.json()) as T;
-  }
-
-  public async _getCoinsListWithMarketData(
-    vsCurrency: string = this._vsCurrency,
-    perPage: number = this._perPage,
-    page: number = this._page
-  ): Promise<ITransformedCoinsMarketData[]> {
-    const url = `${this._apiBase}coins/markets?x_cg_demo_api_key=${this._apiKey}&vs_currency=${vsCurrency}&per_page=${perPage}&page=${page}&sparkline=true&price_change_percentage=1h%2C24h%2C7d%2C14d%2C30d%2C1y&precision=2`;
-    const data = await this.getResource<ICoinsMarketData[]>(url);
+  const getCoinsListWithMarketData = async (
+    vsCurrency: string = _vsCurrency,
+    perPage: number = 30,
+    page: number = 1
+  ): Promise<ITransformedCoinsMarketData[]> => {
+    const url = `${_apiBase}coins/markets?x_cg_demo_api_key=${_apiKey}&vs_currency=${vsCurrency}&per_page=${perPage}&page=${page}&sparkline=true&price_change_percentage=1h%2C24h%2C7d%2C14d%2C30d%2C1y&precision=2`;
+    const data = await request<ICoinsMarketData[]>(url, {});
     const transformedData = transformCoinsListWithMarketData(data);
 
     return transformedData;
-  }
+  };
 
-  public async _getCoinsListLength(): Promise<number> {
-    const url = `${this._apiBase}coins/list?x_cg_demo_api_key=${this._apiKey}`;
-    const data = await this.getResource<ICoinsListData[]>(url);
-
-    return data.length;
-  }
-
-  public async _getCoinHistoricalChartDataById(
+  const getCoinHistoricalChartDataById = async (
     id: string,
     from: number,
     to: number,
-    vsCurrency: string = this._vsCurrency
-  ): Promise<ICoinHistoricalChartDataById> {
-    const url = `${this._apiBase}coins/${id}/market_chart/range?vs_currency=${vsCurrency}&from=${from}&to=${to}&x_cg_demo_api_key=${this._apiKey}`;
-    const data = await this.getResource<ICoinHistoricalChartDataById>(url);
+    vsCurrency: string = _vsCurrency
+  ): Promise<ICoinHistoricalChartDataById> => {
+    const url = `${_apiBase}coins/${id}/market_chart/range?vs_currency=${vsCurrency}&from=${from}&to=${to}&x_cg_demo_api_key=${_apiKey}`;
+    const data = await request<ICoinHistoricalChartDataById>(url, {});
     const labeledData = transformCoinHistoricalChartDataById(data);
 
     return labeledData;
-  }
-}
+  };
 
-export default CoinGeckoService;
+  const getCoinsListLength = async (): Promise<number> => {
+    const url = `${_apiBase}coins/list?x_cg_demo_api_key=${_apiKey}`;
+    const data = await request<ICoinsListData[]>(url, {});
+
+    return data.length;
+  };
+
+  return {
+    loading,
+    error,
+    getCoinsListWithMarketData,
+    getCoinHistoricalChartDataById,
+    getCoinsListLength,
+  };
+};
+
+export default useCoinGeckoService;
