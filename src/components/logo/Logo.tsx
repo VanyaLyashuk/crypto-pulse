477c82a0b1c1e53ee8c6f194810dac196cbfd6bc
VanyaLyashuk <ivan129522@gmail.com>
2024-10-22 11:39:57 +0300

    refactor(Logo): use secondary color variable from tailwind config instead of tailwind utility class

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -5,7 +5,7 @@ const Logo = () => {
     <h1 className="text-[28px] font-pixelify-sans flex items-center gap-[2px]">
       <img className="w-9" src={coinImg} alt="coin image" />
       <span className="text-coinGold crypto-shadow">rypto</span>
-      <span className="font-sans font-bold text-teal-500 second-font">pulse</span>
+      <span className="font-sans font-bold text-secondary second-font">pulse</span>
     </h1>
   );
 };
