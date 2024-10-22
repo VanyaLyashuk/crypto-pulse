f924011ab2bf96b9d57afeb7653750a4f7985966
VanyaLyashuk <ivan129522@gmail.com>
2024-10-22 16:30:34 +0300

    refactor(Logo): decrease font size to 26px

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -2,7 +2,7 @@ import coinImg from "../../assets/images/coin.png";
 
 const Logo = () => {
   return (
-    <h1 className="text-[28px] font-pixelify-sans flex items-center gap-[2px]">
+    <h1 className="flex items-center gap-1 text-[26px] font-pixelify-sans">
       <img className="w-9" src={coinImg} alt="coin image" />
       <span className="text-primary-logo crypto-shadow">rypto</span>
       <span className="font-sans font-bold text-secondary second-font">pulse</span>
