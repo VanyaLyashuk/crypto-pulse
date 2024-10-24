88e10a262b41249c9387587a981a66770008746f
VanyaLyashuk <ivan129522@gmail.com>
2024-10-24 17:49:12 +0300

    refactor(App): fix routes issue when open modal with url

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,25 @@ const App: React.FC = () => {
   const state = location.state as { backgroundLocation?: Location };
 
   return (
-      <>
-        <Routes location={state?.backgroundLocation || location}>
-          <Route path="/" element={<Home />} />
+    <>
+      <Routes location={state?.backgroundLocation || location}>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/coin/:coinId"
+          element={
+            <>
+              <Home />
+              <CoinInfo />
+            </>
+          }
+        />
+      </Routes>
+      {state?.backgroundLocation && (
+        <Routes>
           <Route path="/coin/:coinId" element={<CoinInfo />} />
         </Routes>
-        {state?.backgroundLocation && (
-          <Routes>
-            <Route path="/coin/:coinId" element={<CoinInfo />} />
-          </Routes>
-        )}
-      </>
+      )}
+    </>
   );
 };
 
