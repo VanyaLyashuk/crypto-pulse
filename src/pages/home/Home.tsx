35a73a4aa9aded4ec1c2359d5150e608d4223ac1
VanyaLyashuk <ivan129522@gmail.com>
2024-10-26 13:23:36 +0300

    feat: add search component to home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,15 @@
 import CryptoTable from "../../components/cryptoTable/CryptoTable";
 import ErrorBoundary from "../../components/errorBoundary/ErrorBoundary";
 import Header from "../../components/header/Header";
+import Search from "../../components/search/Search";
 
 const Home: React.FC = () => {
   return (
     <div className="pt-[64px] pb-5">
       <Header />
+      <div className="container flex justify-center mt-10">
+        <Search />
+      </div>
       <ErrorBoundary>
         <CryptoTable />
       </ErrorBoundary>
