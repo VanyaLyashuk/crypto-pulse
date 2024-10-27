b7af8e8891eef69342966a64825d0745bc70c901
VanyaLyashuk <ivan129522@gmail.com>
2024-10-27 17:37:18 +0200

    feat: use TypeWriter component in Home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,12 +2,21 @@ import CryptoTable from "../../components/cryptoTable/CryptoTable";
 import ErrorBoundary from "../../components/errorBoundary/ErrorBoundary";
 import Header from "../../components/header/Header";
 import Search from "../../components/search/Search";
+import TypeWriter from "../../components/typeWriter/TypeWriter";
 
 const Home: React.FC = () => {
   return (
     <div className="pt-[64px] pb-5">
       <Header />
-      <div className="container flex justify-center mt-10">
+      <div className="container flex flex-col justify-center mt-10">
+        <div>
+          <TypeWriter
+            sentences={[
+              "Start typing to search, or use the table for quick access",
+              "Input a coin name or browse the list for detailed data",
+            ]}
+          />
+        </div>
         <Search />
       </div>
       <ErrorBoundary>
