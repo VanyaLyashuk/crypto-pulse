d93e30dd95972804b244f0d21e76a160d4d619aa
VanyaLyashuk <ivan129522@gmail.com>
2024-10-28 16:44:23 +0200

    feat: add animated bar to promo title

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import CryptoTable from "../../components/cryptoTable/CryptoTable";
 import ErrorBoundary from "../../components/errorBoundary/ErrorBoundary";
 import Header from "../../components/header/Header";
@@ -10,7 +11,23 @@ const Home: React.FC = () => {
       <Header />
       <div className="container flex flex-col justify-center pt-10 md:pt-20">
         <div>
-          <h1 className="mb-2 text-3xl font-bold text-center font-chakra-petch sm:text-4xl md:text-5xl">YOUR ULTIMATE CRYPTO<br /> INSIGHT PLATFORM</h1>
+          <h1 className="mb-2 text-3xl font-bold text-center font-chakra-petch sm:text-4xl md:text-5xl">
+            YOUR{" "}
+            <span className="relative">
+              ULTIMATE
+              <motion.span
+                initial={{ width: "0%", opacity: 0 }}
+                animate={{ width: "101%", opacity: 1 }}
+                transition={{
+                  ease: "linear",
+                  delay: 2,
+                }}
+                className="absolute bottom-[18%] left-0 h-[24%] bg-secondary z-[-1]"
+              />
+            </span>{" "}
+            CRYPTO
+            <br /> INSIGHT PLATFORM
+          </h1>
           <TypeWriter
             sentences={[
               "Start typing to search, or use the table for quick access",
