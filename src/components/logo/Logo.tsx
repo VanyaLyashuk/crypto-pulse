15024ed72aeb32a5216e42d75cbac935f6a99efd
VanyaLyashuk <ivan129522@gmail.com>
2024-10-28 18:00:27 +0200

    refactor(Logo): remove unused classes

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -5,7 +5,7 @@ const Logo = () => {
     <h1 className="flex items-center gap-1 text-[26px] font-pixelify-sans">
       <img className="w-9" src={coinImg} alt="coin image" />
       <span className="text-primary-logo crypto-shadow">rypto</span>
-      <span className="font-sans font-bold text-secondary second-font">pulse</span>
+      <span className="font-sans font-bold text-secondary">pulse</span>
     </h1>
   );
 };
