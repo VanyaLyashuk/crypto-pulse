21b59c55820c76e59b40f840c398c22c1f7f51e3
VanyaLyashuk <ivan129522@gmail.com>
2024-10-30 10:20:53 +0200

    fix(CryptoTableSkeleton): correct rows array generation

--- src/components/cryptoTableSkeleton/CryptoTableSkeleton.tsx
+++ src/components/cryptoTableSkeleton/CryptoTableSkeleton.tsx
@@ -1,60 +1,56 @@
 import React from "react";
+import { useShallow } from "zustand/react/shallow";
 import { ICryptoTableSkeletonProps } from "../../models";
+import useFavoritesStore from "../../store/favorites.store";
 
 const CryptoTableSkeleton: React.FC<ICryptoTableSkeletonProps> = ({
   rowsPerPage,
 }) => {
-  const rows = Array.from({ length: rowsPerPage });
+  const { favorites, showFavorites } = useFavoritesStore(
+    useShallow((state) => ({
+      favorites: state.favorites,
+      showFavorites: state.showFavorites,
+    }))
+  );
+
+  let rows = Array.from({
+    length: showFavorites ? favorites.length : rowsPerPage,
+  });
 
   return (
     <div className="overflow-x-auto max-w-[1300px] m-auto">
       <table className="crypto-table min-w-[129px] shadow table-auto sm:rounded-lg">
         <thead>
           <tr>
-            <th className="px-2 py-[17px] bg-primary-bg "></th>
-            <th className="px-2 py-[17px] bg-primary-bg ">
+            <th className="px-2 py-3 bg-primary-bg "></th>
+            <th className="px-2 py-3 bg-primary-bg ">
               <div className="w-6 h-6 rounded-sm bg-skeleton-bg animate-pulse"></div>
             </th>
             <th
-              className="px-2 py-[17px] bg-primary-bg  w-full"
+              className="w-full px-2 py-3 bg-primary-bg"
               style={{ minWidth: "200px" }}
             >
               <div className="h-6 rounded-sm bg-skeleton-bg w-14 animate-pulse"></div>
             </th>
-            <th className="px-2 py-[17px] bg-primary-bg ">
+            <th className="px-2 py-3 bg-primary-bg ">
               <div className="h-6 ml-auto rounded-sm bg-skeleton-bg w-14 animate-pulse"></div>
             </th>
-            <th className="px-2 py-[17px] bg-primary-bg" style={{ width: "71px" }}>
+            <th className="px-2 py-3 bg-primary-bg" style={{ width: "71px" }}>
               <div className="w-8 h-6 ml-auto rounded-sm bg-skeleton-bg animate-pulse"></div>
             </th>
-            <th
-              className="px-2 py-[17px] bg-primary-bg "
-              style={{ width: "71px" }}
-            >
+            <th className="px-2 py-3 bg-primary-bg " style={{ width: "71px" }}>
               <div className="w-8 h-6 ml-auto rounded-sm bg-skeleton-bg animate-pulse"></div>
             </th>
-            <th
-              className="px-2 py-[17px] bg-primary-bg "
-              style={{ width: "71px" }}
-            >
+            <th className="px-2 py-3 bg-primary-bg " style={{ width: "71px" }}>
               <div className="w-8 h-6 ml-auto rounded-sm bg-skeleton-bg animate-pulse"></div>
             </th>
-            <th
-              className="px-2 py-[17px] bg-primary-bg "
-              style={{ width: "145px" }}
-            >
+            <th className="px-2 py-3 bg-primary-bg " style={{ width: "145px" }}>
               <div className="w-24 h-6 ml-auto rounded-sm bg-skeleton-bg animate-pulse"></div>
             </th>
-            <th
-              className="px-2 py-[17px] bg-primary-bg "
-              style={{ width: "145px" }}
-            >
+            <th className="px-2 py-3 bg-primary-bg " style={{ width: "145px" }}>
               <div className="w-24 h-6 ml-auto rounded-sm bg-skeleton-bg animate-pulse"></div>
             </th>
-            <th
-              className="px-2 py-[17px] bg-primary-bg "
-              style={{ width: "151px" }}
-            >
+            <th className="px-2 py-3 bg-primary-bg " style={{ width: "151px" }}>
               <div className="w-24 h-6 ml-auto rounded-sm bg-skeleton-bg animate-pulse"></div>
             </th>
           </tr>
