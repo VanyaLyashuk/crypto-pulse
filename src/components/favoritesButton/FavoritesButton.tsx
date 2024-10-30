fa81e5ff063e9d8bf2736b161eb4366033102872
VanyaLyashuk <ivan129522@gmail.com>
2024-10-30 10:19:20 +0200

    refactor(FavoritesButton): extend button with different logic for head and table rows

--- src/components/favoritesButton/FavoritesButton.tsx
+++ src/components/favoritesButton/FavoritesButton.tsx
@@ -1,12 +1,72 @@
-import { useState } from "react";
+import { clsx } from "clsx";
+import { useEffect, useState } from "react";
 import { GoStar, GoStarFill } from "react-icons/go";
+import {
+  IFavoritesButtonProps,
+  IFavoritesButtonViewProps,
+  TButtonClickHandler,
+} from "../../models";
+import useFavoritesStore from "../../store/favorites.store";
 
-const FavoritesButton = () => {
+const FavoritesButton: React.FC<IFavoritesButtonProps> = ({
+  coinId,
+  isHeader,
+}) => {
   const [isFavorite, setIsFavorite] = useState<boolean>(false);
+  const { favorites, toggleFavorites, showFavorites, toggleShowFavorites } =
+    useFavoritesStore();
+
+  useEffect(() => {
+    if (coinId) {
+      setIsFavorite(favorites.includes(coinId));
+    }
+  }, [favorites, coinId]);
+
+  const onToggleFavorites: TButtonClickHandler = (event) => {
+    event.stopPropagation();
+
+    if (coinId) {
+      toggleFavorites(coinId);
+    }
+
+    setIsFavorite(!isFavorite);
+  };
+
+  const handleFavorites: TButtonClickHandler = (event) => {
+    event.stopPropagation();
+
+    setIsFavorite(!isFavorite);
+    toggleShowFavorites();
+  };
+
+  const button = isHeader ? (
+    <ButtonView
+      handleClick={(event) => handleFavorites(event)}
+      isFilled={showFavorites}
+      disabled={!favorites.length}
+    />
+  ) : (
+    <ButtonView
+      handleClick={(event) => onToggleFavorites(event)}
+      isFilled={isFavorite}
+    />
+  );
+
+  return button;
+};
+
+const ButtonView: React.FC<IFavoritesButtonViewProps> = ({
+  handleClick,
+  isFilled,
+  disabled,
+}) => {
+  const buttonClasses = clsx("block mx-auto focus-visible-outline", {
+    "text-gray-300 dark:text-gray-700": disabled,
+  });
 
   return (
-    <button onClick={() => setIsFavorite(!isFavorite)}>
-      {isFavorite ? <GoStarFill /> : <GoStar />}
+    <button onClick={handleClick} className={buttonClasses} disabled={disabled}>
+      {isFilled ? <GoStarFill className="fill-primary-logo" /> : <GoStar />}
     </button>
   );
 };
