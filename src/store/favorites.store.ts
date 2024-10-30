4961033f02b4c6ae26880432fe00251cd3fa3ea0
VanyaLyashuk <ivan129522@gmail.com>
2024-10-30 10:16:58 +0200

    feat: create favorites store

---
+++ src/store/favorites.store.ts
@@ -0,0 +1,27 @@
+import { create } from "zustand";
+import { devtools } from "zustand/middleware";
+import { IFavoritesStore } from "../models";
+
+const useFavoritesStore = create<IFavoritesStore>()(
+  devtools(
+    (set) => ({
+      favorites: JSON.parse(localStorage.getItem("favorites") || "[]"),
+      toggleFavorites: (coinId: string) =>
+        set((state) => {
+          const updatedFavorites = state.favorites.includes(coinId)
+            ? state.favorites.filter((id) => id !== coinId)
+            : [...state.favorites, coinId];
+          localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
+          return { favorites: updatedFavorites };
+        }),
+      showFavorites: false,
+      toggleShowFavorites: () =>
+        set((state) => ({
+          showFavorites: !state.showFavorites,
+        })),
+    }),
+    { name: "FavoritesStore" }
+  )
+);
+
+export default useFavoritesStore;
