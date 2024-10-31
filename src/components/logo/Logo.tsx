eb1b474cab6af81ece58cc37ef9d45bf9b848b47
VanyaLyashuk <ivan129522@gmail.com>
2024-10-31 10:11:24 +0200

    refactor(Logo): replace h1 with div in logo component

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -2,11 +2,11 @@ import coinImg from "../../assets/images/coin.png";
 
 const Logo = () => {
   return (
-    <h1 className="flex items-center gap-1 text-[26px] font-pixelify-sans">
+    <div className="flex items-center gap-1 text-[26px] font-pixelify-sans">
       <img className="w-9" src={coinImg} alt="coin image" />
       <span className="text-primary-logo crypto-shadow">rypto</span>
       <span className="font-sans font-bold text-secondary">pulse</span>
-    </h1>
+    </div>
   );
 };
 
