804766e7a1c5e22a1ceb290ffda0665934ac1293
VanyaLyashuk <ivan129522@gmail.com>
2024-11-01 10:12:27 +0200

    refacrot(CryptoTableSkeleton): remove inline styles, add Tailwind classes for consistent styling

--- src/components/cryptoTableSkeleton/CryptoTableSkeleton.tsx
+++ src/components/cryptoTableSkeleton/CryptoTableSkeleton.tsx
@@ -26,31 +26,28 @@ const CryptoTableSkeleton: React.FC<ICryptoTableSkeletonProps> = ({
             <th className="px-2 py-3 bg-primary-bg ">
               <div className="w-6 h-6 rounded-sm bg-skeleton-bg animate-pulse"></div>
             </th>
-            <th
-              className="w-full px-2 py-3 bg-primary-bg"
-              style={{ minWidth: "200px" }}
-            >
+            <th className="w-full px-2 py-3 bg-primary-bg min-w-[200px]">
               <div className="h-6 rounded-sm bg-skeleton-bg w-14 animate-pulse"></div>
             </th>
             <th className="px-2 py-3 bg-primary-bg ">
               <div className="h-6 ml-auto rounded-sm bg-skeleton-bg w-14 animate-pulse"></div>
             </th>
-            <th className="px-2 py-3 bg-primary-bg" style={{ width: "71px" }}>
+            <th className="px-2 py-3 bg-primary-bg w-[71px]">
               <div className="w-8 h-6 ml-auto rounded-sm bg-skeleton-bg animate-pulse"></div>
             </th>
-            <th className="px-2 py-3 bg-primary-bg " style={{ width: "71px" }}>
+            <th className="px-2 py-3 bg-primary-bg w-[71px]">
               <div className="w-8 h-6 ml-auto rounded-sm bg-skeleton-bg animate-pulse"></div>
             </th>
-            <th className="px-2 py-3 bg-primary-bg " style={{ width: "71px" }}>
+            <th className="px-2 py-3 bg-primary-bg w-[71px]">
               <div className="w-8 h-6 ml-auto rounded-sm bg-skeleton-bg animate-pulse"></div>
             </th>
-            <th className="px-2 py-3 bg-primary-bg " style={{ width: "145px" }}>
+            <th className="px-2 py-3 bg-primary-bg w-[145px]">
               <div className="w-24 h-6 ml-auto rounded-sm bg-skeleton-bg animate-pulse"></div>
             </th>
-            <th className="px-2 py-3 bg-primary-bg " style={{ width: "145px" }}>
+            <th className="px-2 py-3 bg-primary-bg w-[145px]">
               <div className="w-24 h-6 ml-auto rounded-sm bg-skeleton-bg animate-pulse"></div>
             </th>
-            <th className="px-2 py-3 bg-primary-bg " style={{ width: "151px" }}>
+            <th className="px-2 py-3 bg-primary-bg w-[151px]">
               <div className="w-24 h-6 ml-auto rounded-sm bg-skeleton-bg animate-pulse"></div>
             </th>
           </tr>
@@ -58,40 +55,37 @@ const CryptoTableSkeleton: React.FC<ICryptoTableSkeletonProps> = ({
         <tbody className="bg-primary-bg ">
           {rows.map((_, index) => (
             <tr key={index}>
-              <td className="px-2 py-[17px]" style={{ width: "32px" }}>
+              <td className="px-2 py-[17px] w-[32px]">
                 <div className="w-4 h-4 rounded-sm bg-skeleton-bg animate-pulse"></div>
               </td>
-              <td className="px-2 py-[17px]" style={{ width: "50px" }}>
+              <td className="px-2 py-[17px] w-[50px]">
                 <div className="w-6 h-6 rounded-sm bg-skeleton-bg animate-pulse"></div>
               </td>
-              <td
-                className="px-2 py-[17px] w-full"
-                style={{ minWidth: "200px" }}
-              >
+              <td className="px-2 py-[17px] w-full min-w-[200px]">
                 <div className="flex items-center">
                   <div className="w-6 h-6 mr-2 rounded-full bg-skeleton-bg animate-pulse"></div>
                   <div className="flex-1 h-6 bg-skeleton-bg   max-w-[280px] animate-pulse rounded-sm"></div>
                 </div>
               </td>
-              <td className="px-2 py-[17px]" style={{ width: "145px" }}>
+              <td className="px-2 py-[17px] w-[145px]">
                 <div className="w-[129px] h-6 ml-auto bg-skeleton-bg   animate-pulse rounded-sm"></div>
               </td>
-              <td className="px-2 py-[17px]" style={{ width: "71px" }}>
+              <td className="px-2 py-[17px] w-[71px]">
                 <div className="h-6 rounded-sm bg-skeleton-bg w-14 animate-pulse"></div>
               </td>
-              <td className="px-2 py-[17px]" style={{ width: "71px" }}>
+              <td className="px-2 py-[17px] w-[71px]">
                 <div className="h-6 rounded-sm bg-skeleton-bg w-14 animate-pulse"></div>
               </td>
-              <td className="px-2 py-[17px]" style={{ width: "71px" }}>
+              <td className="px-2 py-[17px] w-[71px]">
                 <div className="h-6 rounded-sm bg-skeleton-bg w-14 animate-pulse"></div>
               </td>
-              <td className="px-2 py-[17px]" style={{ width: "145px" }}>
+              <td className="px-2 py-[17px] w-[145px]">
                 <div className="w-[129px] h-6 bg-skeleton-bg   animate-pulse rounded-sm"></div>
               </td>
-              <td className="px-2 py-[17px]" style={{ width: "145px" }}>
+              <td className="px-2 py-[17px] w-[145px]">
                 <div className="w-[129px] h-6 bg-skeleton-bg   animate-pulse rounded-sm"></div>
               </td>
-              <td className="px-2 py-[17px]" style={{ width: "151px" }}>
+              <td className="px-2 py-[17px] w-[151px]">
                 <div className="w-[135px] h-6 bg-skeleton-bg   animate-pulse rounded-sm"></div>
               </td>
             </tr>
