ad31af61310565367947df235b2ac1f465346d48
VanyaLyashuk <ivan129522@gmail.com>
2024-11-04 10:40:23 +0200

    refactor: add lazy loading with Suspense and improve theme preload

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
+import { FC, lazy, Suspense } from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
-import CoinInfo from "./pages/coinInfo/CoinInfo";
-import Home from "./pages/home/Home";
+import SpinnerIcon from "./components/UI/SpinnerIcon";
 
-const App: React.FC = () => {
+const CoinInfo = lazy(() => import("./pages/coinInfo/CoinInfo"));
+const Home = lazy(() => import("./pages/home/Home"));
+
+const App: FC = () => {
   const location = useLocation();
   const state = location.state as { backgroundLocation?: Location };
 
   return (
-    <>
+    <Suspense
+      fallback={
+        <div className="grid h-screen place-content-center bg-primary-bg">
+          <SpinnerIcon />
+        </div>
+      }
+    >
       <Routes location={state?.backgroundLocation || location}>
         <Route path="/" element={<Home />} />
         <Route
@@ -25,7 +34,7 @@ const App: React.FC = () => {
           <Route path="/coin/:coinId" element={<CoinInfo />} />
         </Routes>
       )}
-    </>
+    </Suspense>
   );
 };
 
