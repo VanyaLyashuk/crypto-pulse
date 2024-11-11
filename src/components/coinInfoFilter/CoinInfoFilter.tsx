5060ea93cbf1631c862407c24f0d00807b913f98
VanyaLyashuk <ivan129522@gmail.com>
2024-11-11 17:23:28 +0200

    refactor(CoinInfoFilter): replace old filter button logic with CoinInfoFilterButton component

--- src/components/coinInfoFilter/CoinInfoFilter.tsx
+++ src/components/coinInfoFilter/CoinInfoFilter.tsx
@@ -1,89 +1,13 @@
-import clsx from "clsx";
-import debounce from "debounce";
-import { BsCalendar2 } from "react-icons/bs";
-import { useShallow } from "zustand/react/shallow";
-import {
-  ICoinInfoFilterProps,
-  TCoinInfoMetric,
-  TCoinInfoTimeRange,
-} from "../../models";
-import useCoinInfoStore from "../../store/coinInfo.store";
-import { calcStartDate } from "../../utils/CryptoTableUtils";
-import CoinInfoDatepicker from "../coinInfoDatePicker/CoinInfoDatePicker";
+import { ICoinInfoFilterProps } from "../../models";
+import CoinInfoFilterButton from "../coinInfoFilterButton/CoinInfoFilterButton";
 
 const CoinInfoFilter: React.FC<ICoinInfoFilterProps> = ({
   filterOptions,
   activeFilter,
 }) => {
-  const {
-    isDatepickerOpen,
-    setIsDatepickerOpen,
-    setSelectedMetric,
-    setSelectedTimeRange,
-    startDate,
-    setStartDate,
-    setEndDate,
-  } = useCoinInfoStore(
-    useShallow((state) => ({
-      isDatepickerOpen: state.isDatepickerOpen,
-      setIsDatepickerOpen: state.setIsDatepickerOpen,
-      setSelectedMetric: state.setSelectedMetric,
-      setSelectedTimeRange: state.setSelectedTimeRange,
-      startDate: state.startDate,
-      setStartDate: state.setStartDate,
-      setEndDate: state.setEndDate,
-    }))
-  );
-
-  const onFilterChange = debounce((filter: TCoinInfoMetric | TCoinInfoTimeRange) => {
-    const isMetric = filter === "Price" || filter === "Market Cap";
-    const isDateRange = filter === "date range";
-  
-    if (isMetric) {
-      setSelectedMetric(filter);
-    } else {
-      setSelectedTimeRange(filter);
-  
-      if (startDate && !isDateRange) {
-        const endDate = new Date();
-        const startDate = calcStartDate(filter, endDate);
-        setStartDate(startDate);
-        setEndDate(endDate);
-      }
-    }
-  }, 100);
-
-  const buttons = filterOptions.map((filter) => {
-    const btnClasses = clsx("px-1.5 py-1 sm:px-2 sm:py-1.5 text-sm font-medium", {
-      "bg-primary-bg rounded-md shadow-sm ": activeFilter === filter,
-    });
-
-    return filter === "date range" ? (
-      <div className="relative inline-block top-[1px] sm:top-0" key={filter}>
-        <button
-          className={btnClasses}
-          onClick={() => {
-            onFilterChange(filter);
-            setIsDatepickerOpen(!isDatepickerOpen);
-          }}
-        >
-          <BsCalendar2 className="w-4 h-4 sm:h-5" />
-        </button>
-        {isDatepickerOpen && <CoinInfoDatepicker />}
-      </div>
-    ) : (
-      <button
-        key={filter}
-        className={btnClasses}
-        onClick={() => {
-          onFilterChange(filter)
-          setIsDatepickerOpen(false);
-        }}
-      >
-        {filter}
-      </button>
-    );
-  });
+  const buttons = filterOptions.map((filter) => (
+    <CoinInfoFilterButton filter={filter} activeFilter={activeFilter} />
+  ));
 
   return (
     <div className="flex items-center gap-[2px] sm:gap-1 p-1.5 bg-filter-bg rounded-md md:rounded-lg">
