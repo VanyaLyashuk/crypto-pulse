36c589f44470b68745c1a47a0941a884d3493b91
VanyaLyashuk <ivan129522@gmail.com>
2024-11-11 18:10:48 +0200

    refactor(CoinInfoListItem): replace old tooltip logic witch CoinInfoTooltip component

--- src/components/coinInfoListItem/CoinInfoListItem.tsx
+++ src/components/coinInfoListItem/CoinInfoListItem.tsx
@@ -1,64 +1,44 @@
-import clsx from "clsx";
-import { useState } from "react";
-import { PiInfo } from "react-icons/pi";
 import useFormattedSmallCurrency from "../../hooks/useFormattedSmallCurrency";
 import { ICoinListItemProps } from "../../models";
+import CoinInfoTooltip from "../coinInfoTooltip/CoinInfoTooltip";
 import PriceChangeIndicator from "../priceChangeIndicator/PriceChangeIndicator";
 
 const CoinInfoListItem: React.FC<ICoinListItemProps> = ({ value }) => {
-  const [isTooltipOpen, setIsTooltipOpen] = useState<boolean>(false);
-
-  const tooltipClasses = clsx(
-    "absolute top-1/2 translate-y-[-50%] left-[22px] bg-primary-bg text-xs shadow-md p-2 rounded-md text-nowrap",
-    {
-      "opacity-0 hidden": !isTooltipOpen,
-    },
-    {
-      "opacity-1 visible": isTooltipOpen,
-    }
-  );
-
   const liClasses =
     "flex items-start justify-between py-3 font-medium border-b border-b-delimeter-color last:border-b-0 last:pb-0";
 
+  const { label, price } = value;
+
+  const formattedPrice = Array.isArray(price)
+    ? `${useFormattedSmallCurrency(
+        price[0],
+        "$"
+      )} - ${useFormattedSmallCurrency(price[1], "$")}`
+    : useFormattedSmallCurrency(price as number, "$");
+
   if ("percentage" in value) {
+    const { percentage, date } = value;
+
     return (
       <li className={liClasses}>
-        <div
-          onClick={() => setIsTooltipOpen(!isTooltipOpen)}
-          className="flex items-center gap-1 cursor-pointer text-secondary-text"
-        >
-          <h5 className="">{value.label}</h5>{" "}
-          <div className="relative">
-            <PiInfo className="relative top-[1px]" />
-            <div className={tooltipClasses}>{value.date}</div>
-          </div>
+        <div className="flex items-center gap-1 text-secondary-text">
+          <h5 className="">{label}</h5>
+          <CoinInfoTooltip date={date} />
         </div>
         <div className="flex items-center justify-end font-medium gap-x-2">
-          <h4>
-            {typeof value.price === "number"
-              ? useFormattedSmallCurrency(value.price, "$")
-              : value.price}
-          </h4>
-          <PriceChangeIndicator arrowSize="w-2 h-2" value={value.percentage} />
+          <h4>{formattedPrice}</h4>
+          <PriceChangeIndicator arrowSize="w-2 h-2" value={percentage} />
         </div>
-      </li>
-    );
-  } else {
-    return (
-      <li className={liClasses}>
-        <h5 className="mr-2 text-secondary-text lg:mr-4">{value.label}</h5>{" "}
-        {Array.isArray(value.price) ? (
-          <div>
-            {useFormattedSmallCurrency(value.price[0], "$")}-
-            {useFormattedSmallCurrency(value.price[1], "$")}
-          </div>
-        ) : (
-          <div>{value.price}</div>
-        )}
       </li>
     );
   }
+
+  return (
+    <li className={liClasses}>
+      <h5 className="mr-2 text-secondary-text lg:mr-4">{label}</h5>
+      {formattedPrice}
+    </li>
+  );
 };
 
 export default CoinInfoListItem;
