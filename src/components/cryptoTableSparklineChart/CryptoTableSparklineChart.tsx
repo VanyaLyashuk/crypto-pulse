96c1506295f1973b4007e5519321650873ee2986
VanyaLyashuk <ivan129522@gmail.com>
2024-11-12 16:24:39 +0200

    refactor(CryptoTableSparklineChart): move options object outside component and replace hardcoded color values with variables

--- src/components/cryptoTableSparklineChart/CryptoTableSparklineChart.tsx
+++ src/components/cryptoTableSparklineChart/CryptoTableSparklineChart.tsx
@@ -8,58 +8,60 @@ import {
 import React from "react";
 import { Line } from "react-chartjs-2";
 import { ISparklineChartProps } from "../../models";
+import { CHART_COLORS } from "../../utils/CryptoTableUtils";
 
 ChartJS.register(LineElement, CategoryScale, LinearScale, PointElement);
 
-const CryptoTableSparklineChart: React.FC<ISparklineChartProps> = ({ price }) => {
+const options = {
+  responsive: true,
+  maintainAspectRatio: false,
+  scales: {
+    x: {
+      display: false,
+    },
+    y: {
+      display: false,
+    },
+  },
+  plugins: {
+    legend: {
+      display: false,
+    },
+    tooltip: {
+      enabled: false,
+    },
+  },
+  elements: {
+    line: {
+      tension: 0.3,
+    },
+    point: {
+      radius: 0,
+      hoverRadius: 0,
+    },
+  },
+};
+
+const CryptoTableSparklineChart: React.FC<ISparklineChartProps> = ({
+  price,
+}) => {
   const isEmpty = price.length === 0;
   const labels = price.map((_, index) => index.toString());
 
   const isPriceIncreasing = price[price.length - 1] >= price[0];
+
+  const { red, green } = CHART_COLORS;
 
   const chartData = {
     labels,
     datasets: [
       {
         data: price,
-        borderColor: isPriceIncreasing
-          ? "rgb(34, 197, 94)"
-          : "rgb(220, 38, 38)",
-        backgroundColor: "rgba(0, 0, 0, 0)",
+        borderColor: isPriceIncreasing ? green : red,
         borderWidth: 1,
         pointRadius: 0,
       },
     ],
-  };
-
-  const options = {
-    responsive: true,
-    maintainAspectRatio: false,
-    scales: {
-      x: {
-        display: false,
-      },
-      y: {
-        display: false,
-      },
-    },
-    plugins: {
-      legend: {
-        display: false,
-      },
-      tooltip: {
-        enabled: false,
-      },
-    },
-    elements: {
-      line: {
-        tension: 0.3,
-      },
-      point: {
-        radius: 0,
-        hoverRadius: 0,
-      }
-    },
   };
 
   return (
