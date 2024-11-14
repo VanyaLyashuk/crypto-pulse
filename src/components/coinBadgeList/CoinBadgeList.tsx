2ab82aefda634122e14fabcb3cb34f9be139faf8
VanyaLyashuk <ivan129522@gmail.com>
2024-11-14 11:33:49 +0200

    refactor(Search): extract CoinBadgeList component

---
+++ src/components/coinBadgeList/CoinBadgeList.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+import { ICoinBadgeListProps } from "../../models";
+import CoinBadge from "../CoinBadge/CoinBadge";
+
+const CoinBadgeList: FC<ICoinBadgeListProps> = ({ data }) => {
+  return (
+    <ul className="flex flex-wrap justify-center w-full gap-2 p-2 rounded-md">
+      {data.map((coin, index) => (
+        <CoinBadge key={coin.id} {...coin} index={index} />
+      ))}
+    </ul>
+  );
+};
+
+export default CoinBadgeList;
