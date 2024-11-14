0c4d45b250a29f21a15ea2702d79c768cea999c0
VanyaLyashuk <ivan129522@gmail.com>
2024-11-14 18:28:34 +0200

    refactor(ThemeSwitcher): use new ThemeStore instead of local state

--- src/components/themeSwitcher/ThemeSwitcher.tsx
+++ src/components/themeSwitcher/ThemeSwitcher.tsx
@@ -1,28 +1,21 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { IoSunny } from "react-icons/io5";
 import { MdOutlineDarkMode } from "react-icons/md";
-import { TTheme } from "../../models";
+import useThemeStore from "../../store/theme.store";
 
 const ThemeSwitcher = () => {
-  const [theme, setTheme] = useState<TTheme>(() => {
-    return (localStorage.getItem("theme") as TTheme) || "light";
-  });
+  const { theme, toggleTheme } = useThemeStore();
 
   useEffect(() => {
-    localStorage.setItem("theme", theme);
     const html = document.querySelector("html");
     html?.classList.remove("dark", "light");
     html?.classList.add(theme);
   }, [theme]);
 
-  const changeTheme = () => {
-    setTheme(theme === "light" ? "dark" : "light");
-  };
-
   return (
     <button
       className="p-2 rounded-full shadow-md bg-primary-bg dark:shadow-md focus-visible-outline focus-visible-rounded"
-      onClick={changeTheme}
+      onClick={toggleTheme}
     >
       {theme === "dark" ? (
         <IoSunny className="w-5 h-5" />
