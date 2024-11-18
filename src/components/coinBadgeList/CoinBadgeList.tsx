68539a57805c3caebe0b06ab99933de7fd3d4d49
VanyaLyashuk <ivan129522@gmail.com>
2024-11-18 22:33:17 +0200

    style(CoinBadgeList): remove padding and increase bottom margin

--- src/components/coinBadgeList/CoinBadgeList.tsx
+++ src/components/coinBadgeList/CoinBadgeList.tsx
@@ -4,7 +4,7 @@ import CoinBadge from "../CoinBadge/CoinBadge";
 
 const CoinBadgeList: FC<ICoinBadgeListProps> = ({ data }) => {
   return (
-    <ul className="flex flex-wrap justify-center w-full gap-2 py-2 mb-2 rounded-md">
+    <ul className="flex flex-wrap justify-center w-full gap-2 mb-8 rounded-md md:mb-10 lg:mb-12">
       {data.map((coin, index) => (
         <CoinBadge key={coin.id} {...coin} index={index} />
       ))}
